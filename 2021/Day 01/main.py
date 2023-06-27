e979c6d3945b909052c94a06556ed7f1495083e3
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-06-27 19:20:10 +0100

    2021 Day 1 - Setup

---
+++ 2021/Day 01/main.py
@@ -0,0 +1,18 @@
+"""
+--- Day 1: Sonar Sweep ---
+
+You're minding your own business on a ship at sea when the overboard alarm goes
+off! You rush to see if you can help. Apparently, one of the Elves tripped and
+accidentally sent the sleigh keys flying into the ocean!
+
+Before you know it, you're inside a submarine the Elves keep ready for
+situations like this.
+
+As the submarine drops below the surface of the ocean, it automatically performs
+a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report
+(your puzzle input) appears: each line is a measurement of the sea floor depth
+as the sweep looks further and further away from the submarine.
+
+    Part 1: How many measurements are larger than the previous measurement?
+
+"""
