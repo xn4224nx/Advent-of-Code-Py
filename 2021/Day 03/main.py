5626ed8fbcf54e40aff5b7f505cf1a7c8d814af6
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-05 15:48:45 +0100

    Part 1 - Completed

--- 2021/Day 03/main.py
+++ 2021/Day 03/main.py
@@ -19,3 +19,66 @@ submarine?
 """
 
 
+def str_char_freq(input_str: str) -> dict[str: int]:
+    """
+    Find the frequency of each char in a string.
+    """
+    freq = {}
+
+    for char in input_str:
+        if char not in freq:
+            freq[char] = 1
+        else:
+            freq[char] += 1
+
+    return freq
+
+
+def most_n_least_common(char_freq: dict[str: int]) -> (str, str):
+    """
+    Using a frequency dictionary find the least and most common chars.
+    Return most common and least common in a tuple.
+    """
+
+    return (
+        max(char_freq, key=char_freq.get),
+        min(char_freq, key=char_freq.get)
+    )
+
+
+if __name__ == "__main__":
+
+    # Load the data
+    diag = open("./data/input.txt", "r").read().splitlines()
+
+    # Create storage for the column of bits
+    col_bits = ["" for x in range(len(diag[0]))]
+
+    # Iterate over each line in the diagnostic file
+    for line in diag:
+
+        # Save each bit in the line to each column string
+        for i in range(len(line)):
+            col_bits[i] += line[i]
+
+    # Calculate the Epsilon and Gamma
+    epsilon = ""
+    gamma = ""
+
+    for col in col_bits:
+
+        # Get the frequency of each char in the column
+        char_freqs = str_char_freq(col)
+
+        # get the epsilon and gamma
+        gam, eps = most_n_least_common(char_freqs)
+
+        # Add the characters to the gamma and epsilon
+        epsilon += eps
+        gamma += gam
+
+    # Convert the strings to an integer then a binary
+    epsilon = int(epsilon, 2)
+    gamma = int(gamma, 2)
+
+    print(f"Part 1: The power consumption of the sub = {gamma * epsilon}")
