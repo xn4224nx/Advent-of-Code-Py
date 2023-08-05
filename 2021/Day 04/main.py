bfc62337338f97086918caffbcc8fba36778cd8c
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-05 21:17:58 +0100

    Added day 4 data and structures.

---
+++ 2021/Day 04/main.py
@@ -0,0 +1,19 @@
+"""
+--- Day 4: Giant Squid ---
+
+The submarine has a bingo subsystem to help passengers (currently, you and the
+giant squid) pass the time. It automatically generates a random order in which
+to draw numbers and a random set of boards (your puzzle input).
+
+The winning board is the first to get a row or column.
+
+The score of the winning board can now be calculated. Start by finding the sum
+of all unmarked numbers on that board; in this case, the sum is 188. Then,
+multiply that sum by the number that was just called when the board won, 24, to
+get the final score, 188 * 24 = 4512.
+
+Part 1:
+    To guarantee victory against the giant squid, figure out which board will
+    win first. What will your final score be if you choose that board?
+
+"""
