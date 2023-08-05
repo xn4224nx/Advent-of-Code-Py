4c7e3b9c274929d4f1f4248c6bb271f64967faf4
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-05 14:17:50 +0100

    Added day 03 data & structure.

---
+++ 2021/Day 03/main.py
@@ -0,0 +1,21 @@
+"""
+--- Day 3: Binary Diagnostic ---
+
+The diagnostic report (your puzzle input) consists of a list of binary numbers 
+which, when decoded properly, can tell you many useful things about the 
+conditions of the submarine. The first parameter to check is the power 
+consumption.
+
+You need to use the binary numbers in the diagnostic report to generate two new 
+binary numbers (called the gamma rate and the epsilon rate). The power 
+consumption can then be found by multiplying the gamma rate by the epsilon rate.
+
+Each bit in the gamma rate can be determined by finding the most common bit in 
+the corresponding position of all numbers in the diagnostic report.
+
+Use the binary numbers in your diagnostic report to calculate the gamma rate and
+epsilon rate, then multiply them together. What is the power consumption of the 
+submarine?
+"""
+
+
