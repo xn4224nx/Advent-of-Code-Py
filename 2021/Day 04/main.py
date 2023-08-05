30ef4ebff24c1f52bacc63708ccc842eb9b655fc
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-05 22:39:49 +0100

    Added code to parse the bingo data.

--- 2021/Day 04/main.py
+++ 2021/Day 04/main.py
@@ -17,3 +17,55 @@ Part 1:
     win first. What will your final score be if you choose that board?
 
 """
+
+import numpy as np
+
+
+class BingoGame:
+
+    def __init__(self, data_path:str):
+
+        # Load the data about the initial game setup
+        self.raw_data = open(data_path, "r").read().splitlines()
+
+        # Parse the numbers to be called
+        self.call_numbers = np.array([int(x) for x in self.raw_data[0].split(",")])
+
+        # Storage of the player data
+        self.player_data = []
+
+        # Determine the number of boards
+        self.players = len(self.raw_data)//6
+
+        # Parse the bingo data
+        for i in range(self.players):
+
+            start_idx = 6 * i + 2
+            final_idx = 6 * i + 7
+
+            # Extract the players board data
+            board = self.raw_data[start_idx:final_idx]
+
+            # Convert to a 2d numpy array
+            board = np.array([[int(x) for x in line.split()] for line in board])
+
+            # Store the data
+            self.player_data.append({
+                "board": board,
+                "score": np.full(
+                          shape=(5, 5),
+                          fill_value=False,
+                          dtype=np.bool_)
+            })
+
+
+
+
+if __name__ == "__main__":
+
+    # Sample Game
+    sample = BingoGame("./data/sample.txt")
+
+    print(sample.call_numbers)
+
+
