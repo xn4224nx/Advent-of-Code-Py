3fae312a55ca75231bb471e66e254fb5c7efaddf
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-06 17:51:45 +0100

    Structure and data of day 5.

---
+++ 2021/Day 05/main.py
@@ -0,0 +1,20 @@
+"""
+--- Day 5: Hydrothermal Venture ---
+
+You come across a field of hydrothermal vents on the ocean floor! These vents
+constantly produce large, opaque clouds, so it would be best to avoid them if
+possible.
+
+They tend to form in lines; the submarine helpfully produces a list of nearby
+lines of vents (your puzzle input) for you to review.
+
+Each line of vents is given as a line segment in the format x1,y1 -> x2,y2 where
+x1,y1 are the coordinates of one end the line segment and x2,y2 are the
+coordinates of the other end. These line segments include the points at both
+ends.
+
+Part 1:
+    Consider only horizontal and vertical lines. At how many points do at least
+    two lines overlap?
+
+"""
