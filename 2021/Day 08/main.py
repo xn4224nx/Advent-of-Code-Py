d5fd76a9140d34685667141924dfa9cba9e22972
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-13 23:31:46 +0100

    Part 1 Completed

--- 2021/Day 08/main.py
+++ 2021/Day 08/main.py
@@ -27,3 +27,30 @@ Part 1:
     In the output values, how many times do digits 1, 4, 7, or 8 appear?
 
 """
+
+# Load the raw digit data
+raw_digits = open("./data/input.txt").read().splitlines()
+
+# Parse the digit data
+digits = []
+digit_len_cnt = {x: 0 for x in range(10)}
+
+for line in raw_digits:
+
+    # Extract the signal digits and the output digits
+    signals, outputs = line.split("|")
+
+    # Save the parsed and cleaned digit data
+    digits.append({
+            "signals": [x.strip() for x in signals.split()],
+            "outputs": [x.strip() for x in outputs.split()],
+    })
+
+    # Count the frequency of output digit lengths
+    for digi in digits[-1]["outputs"]:
+        digit_len_cnt[len(digi)] += 1
+
+cnt_1_4_7_8 = (digit_len_cnt[2] + digit_len_cnt[4] +
+               digit_len_cnt[3] + digit_len_cnt[7])
+
+print(f"The answer to part 1: {cnt_1_4_7_8}")
