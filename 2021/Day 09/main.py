e0434e8bb3b3d04e6a96b9237454ea483d1de2ea
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-15 00:25:57 +0100

    Added part 2 question.

--- 2021/Day 09/main.py
+++ 2021/Day 09/main.py
@@ -18,6 +18,10 @@ Part 1:
     Find all of the low points on your heightmap. What is the sum of the risk
     levels of all low points on your heightmap?
 
+Part 2:
+    What do you get if you multiply together the sizes of the three largest
+    basins?
+
 """
 
 import numpy as np
