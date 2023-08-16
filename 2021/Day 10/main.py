90e50c11f78f8cbf6bebac2a17d7d1d3d73e76a6
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-16 21:06:07 +0100

    Added part 2 text.

--- 2021/Day 10/main.py
+++ 2021/Day 10/main.py
@@ -23,6 +23,10 @@ ignore these lines for now.
 Part 1:
     Find the first illegal character in each corrupted line of the navigation
     subsystem. What is the total syntax error score for those errors?
+
+Part 2:
+    Find the completion string for each incomplete line, score the completion
+    strings, and sort the scores. What is the middle score?
 """
 
 open_char = ["(", "[", "{", "<"]
