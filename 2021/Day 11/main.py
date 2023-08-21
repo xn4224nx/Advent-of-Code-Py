6b9dbe503ff24c29cf3c17d6ab26af8b5c19370e
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-21 13:58:55 +0100

    Part 2 question added.

--- 2021/Day 11/main.py
+++ 2021/Day 11/main.py
@@ -24,6 +24,11 @@ step, the following occurs:
 Part 1:
     Given the starting energy levels of the dumbo octopuses in your cavern,
     simulate 100 steps. How many total flashes are there after 100 steps?
+
+Part 2:
+    If you can calculate the exact moments when the octopuses will all flash
+    simultaneously, you should be able to navigate through the cavern. What is
+    the first step during which all octopuses flash?
 """
 
 import numpy as np
