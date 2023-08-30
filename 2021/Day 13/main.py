f42f96d522b7c33bf9452342b97166817908b760
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-30 12:44:51 +0100

    Setup for day 13.

---
+++ 2021/Day 13/main.py
@@ -0,0 +1,16 @@
+"""
+--- Day 13: Transparent Origami ---
+
+The first section is a list of dots on the transparent paper. 0,0 represents the
+top-left coordinate. The first value, x, increases to the right. The second
+value, y, increases downward. So, the coordinate 3,0 is to the right of 0,0, and
+the coordinate 0,7 is below 0,0.
+
+Then, there is a list of fold instructions. Each instruction indicates a line on
+the transparent paper and wants you to fold the paper up (for horizontal y=...
+lines) or left (for vertical x=... lines).
+
+Part 1:
+    How many dots are visible after completing just the first fold instruction
+    on your transparent paper?
+"""
