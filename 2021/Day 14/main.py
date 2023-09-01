f238c0fe239fc69ed5b1086a6ff2fd21d0793553
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-09-01 23:53:34 +0100

    Setup for day 14.

---
+++ 2021/Day 14/main.py
@@ -0,0 +1,17 @@
+"""
+--- Day 14: Extended Polymerization ---
+
+The submarine manual contains instructions for finding the optimal polymer
+formula; specifically, it offers a polymer template and a list of pair insertion
+rules (your puzzle input). You just need to work out what polymer would result
+after repeating the pair insertion process a few times.
+
+The first line is the polymer template - this is the starting point of the
+process. The following section defines the pair insertion rules.
+
+Part 1:
+    Apply 10 steps of pair insertion to the polymer template and find the most
+    and least common elements in the result. What do you get if you take the
+    quantity of the most common element and subtract the quantity of the least
+    common element?
+"""
