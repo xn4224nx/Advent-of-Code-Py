c92d9507422c12c1da574a9d8e1c87554bb7bc6a
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-09-02 03:08:50 +0100

    Boilerplate for day 15.

---
+++ 2021/Day 15/main.py
@@ -0,0 +1,19 @@
+"""
+--- Day 15: Chiton ---
+
+The cavern is large, but has a very low ceiling, restricting your motion to two
+dimensions. The shape of the cavern resembles a square; a quick scan of chiton
+density produces a map of risk level throughout the cave (your puzzle input).
+
+You start in the top left position, your destination is the bottom right
+position, and you cannot move diagonally. The number at each position is its
+risk level; to determine the total risk of an entire path, add up the risk
+levels of each position you enter (that is, don't count the risk level of your
+starting position unless you enter it; leaving it adds no risk to your total).
+
+Your goal is to find a path with the lowest total risk.
+
+Part 1:
+    What is the lowest total risk of any path from the top left to the bottom
+    right?
+"""
