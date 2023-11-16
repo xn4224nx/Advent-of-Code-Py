68523a3ab3c07cfeaee232129f77733524507ebf
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-11-16 23:33:23 +0000

    Dijkstra path finding attempt.

--- 2021/Day 15/main.py
+++ 2021/Day 15/main.py
@@ -19,7 +19,7 @@ Part 1:
 """
 
 import numpy as np
-
+import sys
 
 class ChitonCave:
 
@@ -29,7 +29,9 @@ class ChitonCave:
         self.risk_map = np.array(
             [[int(x) for x in y] for y in
              open(datafile, "r").read().splitlines()])
-
+        
+        print(self.risk_map)
+        
         # Define the start and end points
         self.start = (0, 0)
         self.end = (self.risk_map.shape[0]-1, self.risk_map.shape[1]-1)
@@ -40,20 +42,113 @@ class ChitonCave:
         Dijkstra's algorithm. Use the algorithm listed here:
         https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
         """
+        
+        # Set all points in the cave as sys.maxsize 
+        pnt_dist = {(x, y): sys.maxsize 
+                    for x in range(self.risk_map.shape[0]) 
+                    for y in range(self.risk_map.shape[1])}
 
+        # apart from the origin, which is set as zero
+        pnt_dist[self.start] = 0
+        
         # Mark all points as unvisited
-        unvisited_points = set()
+        unvisited_points = set(pnt_dist.keys()) 
+        
+        # Start at the origin
+        curr_node = self.start
+        
+        # Loop until the destination node has been visited
+        while self.end in unvisited_points:
+            
+            print(f"\ncurrent node = {curr_node}")
+            
+            # For each neighbour calculate the risk
+            neighb_pnts = []
+            for nei_node in self.find_connected_points(curr_node):
+                
+                # Filter down to unvisited neighbours
+                if nei_node not in unvisited_points:
+                    continue
+                
+                # Calculate the risk for that node
+                temp_dist = (
+                    self.risk_map[curr_node] + 
+                    self.risk_map[nei_node]
+                )
+                
+                if temp_dist < pnt_dist[nei_node]:
+                    pnt_dist[nei_node] = temp_dist
 
-        # Set a tentative distance for all points as sys.maxsize apart from
-        # start which is set as zero
-        pnt_dist = {}
+                neighb_pnts.append(nei_node)
+            
+            # Remove the current point from the unvisited ones
+            unvisited_points.remove(curr_node)
+            
+            # select the unvisited node with the smallest distance
+            min_dist = sys.maxsize
+            
+            for node in unvisited_points:
+                if pnt_dist[node] < min_dist:
+                
+                    print(f"\t{node} = {pnt_dist[node]}")
+                    min_dist = pnt_dist[node]
+                    curr_node = node
+            
+            print(f"next current node = {curr_node}")
+        
+        for pnt, dist in pnt_dist.items():
+            print(f"{pnt} {dist}")
 
-    def find_connected_points(
-            self, x_coord: int, y_coord: int) -> list[(int, int)]:
+    def find_all_connected_points(
+            self, centre_point: tuple[int, int]) -> list[(int, int)]:
         """
         For a particular point return a list of the connected points.
         """
-        pass
+        
+        connected_pts = []
+        
+        for x in range(centre_point[0]-1, centre_point[0]+2):
+            for y in range(centre_point[1]-1, centre_point[1]+2):
+                
+                # Check to see if the point is beyond left or right hand edge
+                if x < 0 or x > self.risk_map.shape[0]-1:
+                    continue
+                
+                # Check to see if the point beyond the top or bottom edge
+                if y < 0 or y > self.risk_map.shape[1]-1:
+                    continue
+                
+                # Ignore the point itself
+                if y == centre_point[1] and x == centre_point[0]:
+                    continue                
+                
+                connected_pts.append((x, y))
+        
+        return connected_pts
+
+    def find_connected_points(
+            self, centre_point: tuple[int, int]) -> list[(int, int)]:
+        """
+        For a particular point return a list of the vertically and horizontally 
+        connected points.
+        """
+        
+        connected_pts = []
+        
+        if centre_point[0]-1 >= 0:
+            connected_pts.append((centre_point[0]-1, centre_point[1]))
+        
+        if centre_point[0]+1 < self.risk_map.shape[0]:
+            connected_pts.append((centre_point[0]+1, centre_point[1]))
+        
+        if centre_point[1]-1 >= 0:
+            connected_pts.append((centre_point[0], centre_point[1]-1))
+        
+        if centre_point[1]+1 < self.risk_map.shape[1]:
+            connected_pts.append((centre_point[0], centre_point[1]+1))
+        
+        return connected_pts
+
 
     def calc_path_risk(self, path_points: list[(int, int)]) -> int:
         """
@@ -64,3 +159,5 @@ class ChitonCave:
 
 
 cavern = ChitonCave("./data/sample.txt")
+cavern.find_lowest_risk_path()
+
