d08985533ab6ade23f488decc6cc4300d3b8acc4
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-07-29 13:11:49 +0100

    Part 2 completed.

--- 2015/Day_01/main.py
+++ 2015/Day_01/main.py
@@ -14,13 +14,21 @@ will never find the top or bottom floors.
 
 PART 1: To what floor do the instructions take Santa?
 
+PART 2: What is the position of the character that causes Santa to first
+        enter the basement?
 """
 
 
-def final_floor(direction_file: str) -> int:
+def final_floor(direction_file: str, find_base_idx=False) -> int:
     """
     Using a direction file solely containing ) and ( work out the final
     floor Santa ends up on. We assume Santa starts at floor zero
+
+    The flag `find_base_idx` means the function will return the index of
+    the instruction character that first
+    takes Santa into the basement, ie when the floor first has the value
+    -1.
+
     """
     floor = 0
 
@@ -29,14 +37,19 @@ def final_floor(direction_file: str) -> int:
         dirs = fp.read()
 
     # Iterate over every instruction and move up and down based on it
-    for char in dirs:
+    for idx, char in enumerate(dirs):
         if char == "(":
             floor += 1
         elif char == ")":
             floor -= 1
+
+        # Basement check
+        if find_base_idx and floor == -1:
+            return idx + 1
 
     return floor
 
 
 if __name__ == "__main__":
     print(f"The answer to part 1 = {final_floor('./data/input.txt')}")
+    print(f"The answer to part 2 = {final_floor('./data/input.txt', True)}")
