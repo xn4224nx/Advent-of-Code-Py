95cd8475e34fc93c3a67943c1583df48e7844214
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-08-12 01:38:05 +0100

    Setup for day 9.

---
+++ 2015/Day_09/main.py
@@ -0,0 +1,17 @@
+"""
+--- Day 9: All in a Single Night ---
+Every year, Santa manages to deliver all of his presents in a
+single night.
+
+This year, however, he has some new locations to visit; his
+elves have provided him the distances between every pair of
+locations. He can start and end at any two (different)
+locations he wants, but he must visit each location exactly
+once. What is the shortest distance he can travel to achieve
+this?
+
+PART 1: What is the distance of the shortest route?
+"""
+
+if __name__ == "__main__":
+    pass
