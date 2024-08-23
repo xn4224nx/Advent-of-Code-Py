2d98003cabd69f3b3ddfffdd5f75e0d24784158d
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-08-23 06:33:30 +0100

    Setup for day 14.

---
+++ 2015/Day_14/main.py
@@ -0,0 +1,16 @@
+"""
+--- Day 14: Reindeer Olympics ---
+
+This year is the Reindeer Olympics! Reindeer can fly at high speeds, but must
+rest occasionally to recover their energy. Santa would like to know which of
+his reindeer is fastest, and so he has them race.
+
+Reindeer can only either be flying (always at their top speed) or resting (not
+moving at all), and always spend whole seconds in either state.
+
+PART 1: Given the descriptions of each reindeer (in your puzzle input), after
+        exactly 2503 seconds, what distance has the winning reindeer traveled?
+"""
+
+if __name__ == "__name__":
+    pass
