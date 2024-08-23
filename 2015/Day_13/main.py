d69965986e6bb7aefe049139204bcfbb4083965e
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-08-23 06:13:21 +0100

    Part 1 completed.

--- 2015/Day_13/main.py
+++ 2015/Day_13/main.py
@@ -15,5 +15,77 @@ PART 1: What is the total change in happiness for the optimal seating
         arrangement of the actual guest list?
 """
 
+import re
+from itertools import permutations
+
+
+def parse_relationships(filepath: str) -> (dict[(str, str), int], list[str]):
+    """
+    Open the relationship file and transfrom it into a dictionary of tuples
+    containing the two parties and the effect on the first persons
+    happiness on being seated next to the other person. Also record the unique
+    guests at the party in a list of strings.
+    """
+    re_pat = r"".join(
+        [
+            r"([a-zA-Z]+) would ([a-zA-Z]+) ",
+            r"(\d+) happiness units ",
+            r"by sitting next to ([a-zA-Z]+)",
+        ]
+    )
+
+    rels = {}
+    guests = set()
+    happ = re.compile(re_pat)
+
+    with open(filepath) as fp:
+        for line in fp.readlines():
+            mat = happ.match(line)
+
+            if mat.group(2) == "gain":
+                change = int(mat.group(3))
+            else:
+                change = -1 * int(mat.group(3))
+
+            # Record relationships
+            rels[(mat.group(1), mat.group(4))] = change
+
+            # Record the guests
+            guests.add(mat.group(1))
+            guests.add(mat.group(4))
+
+    return rels, list(guests)
+
+
+def score_arrange(rels: dict[(str, str), int], guests: str) -> int:
+    """
+    Determine the happiness score of a particular seating arrangement.
+    """
+    happy_sum = 0
+
+    for i in range(len(guests)):
+
+        comp_0 = i
+        comp_1 = i + 1
+
+        # Compare the first & last guest relationships
+        if comp_1 == len(guests):
+            comp_1 = 0
+
+        happy_sum += rels[(guests[comp_0], guests[comp_1])]
+        happy_sum += rels[(guests[comp_1], guests[comp_0])]
+
+    return happy_sum
+
+
+def find_max_happy(rels: dict[(str, str), int], guests: list[str]) -> int:
+    """
+    Find the seating arrangement that gives the maximum happiness by testing
+    each combination of guest seating.
+    """
+    return max([score_arrange(rels, comb) for comb in permutations(guests)])
+
+
 if __name__ == "__main__":
-    pass
+    rels, guests = parse_relationships("./data/input.txt")
+    print(f"Part 1 = {find_max_happy(rels, guests)}")
--- 2015/Day_13/tests/test_main.py
+++ 2015/Day_13/tests/test_main.py
@@ -5,5 +5,29 @@ Tests for functions in the main script.
 import main
 
 
-def test_exp_1():
-    pass
+def test_exp_parse_1():
+    rels, guests = main.parse_relationships("./data/example_01.txt")
+    assert set(guests) == set(["David", "Alice", "Bob", "Carol"])
+
+
+def test_exp_parse_2():
+    rels, guests = main.parse_relationships("./data/example_01.txt")
+    assert rels == {
+        ("Alice", "Bob"): 54,
+        ("Alice", "Carol"): -79,
+        ("Alice", "David"): -2,
+        ("Bob", "Alice"): 83,
+        ("Bob", "Carol"): -7,
+        ("Bob", "David"): -63,
+        ("Carol", "Alice"): -62,
+        ("Carol", "Bob"): 60,
+        ("Carol", "David"): 55,
+        ("David", "Alice"): 46,
+        ("David", "Bob"): -7,
+        ("David", "Carol"): 41,
+    }
+
+
+def test_max_happy():
+    rels, guests = main.parse_relationships("./data/example_01.txt")
+    assert main.find_max_happy(rels, guests) == 330
