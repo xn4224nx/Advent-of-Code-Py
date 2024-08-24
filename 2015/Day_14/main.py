f5608815dbe9d7c73d85644dda5fe7fbacdb2fc2
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-08-24 06:43:17 +0100

    Part 1 completed.

--- 2015/Day_14/main.py
+++ 2015/Day_14/main.py
@@ -12,5 +12,70 @@ PART 1: Given the descriptions of each reindeer (in your puzzle input), after
         exactly 2503 seconds, what distance has the winning reindeer traveled?
 """
 
-if __name__ == "__name__":
-    pass
+import re
+
+
+def parse_data(file_path: str) -> dict[str : dict[str, int]]:
+    """
+    Read the reindeer data file and return a list of the reindeer details.
+    """
+    rein_re = re.compile(
+        "".join([r"([A-Za-z]+) [\D]+ ([\d]+) [\D]+ ", r"([\d]+) [\D]+ ([\d]+)"])
+    )
+    all_results = {}
+
+    with open(file_path) as fp:
+        for line in fp.readlines():
+            matches = rein_re.match(line)
+
+            all_results[matches.group(1)] = {
+                "fly_speed": int(matches.group(2)),
+                "fly_time": int(matches.group(3)),
+                "rest_time": int(matches.group(4)),
+            }
+
+    return all_results
+
+
+def dist_trav(reindeer: dict[str, int], total_time):
+    """
+    A generator to determine how far each the reindeer has traveled up to this
+    second. Each iteration is assumed to be one second passed.
+    """
+    secs_passed = 0
+    speed = reindeer["fly_speed"]
+    f_time = reindeer["fly_time"]
+    r_time = reindeer["rest_time"]
+    cycle_time = f_time + r_time
+
+    while secs_passed <= total_time:
+        secs_passed += 1
+
+        # Calculate the total number of rest fly cycles that have been completed
+        whole_cycles = secs_passed // cycle_time
+
+        dist = whole_cycles * speed * f_time
+
+        # Calculate the time into the current cycle
+        rem_time = total_time - whole_cycles * (f_time + r_time)
+
+        dist += min(rem_time, f_time) * speed
+
+        yield dist
+
+
+def race_winner_dist(all_rein: dict[str : dict[str, int]], total_time: int) -> int:
+    """
+    Find the distance traveled by the winning Reindeer.
+    """
+    dists = []
+
+    for rein in all_rein.values():
+        dists.append([x for x in dist_trav(rein, total_time)][-1])
+
+    return max(dists)
+
+
+if __name__ == "__main__":
+    rein_data = parse_data("./data/input.txt")
+    print(f"Part 1 = {race_winner_dist(rein_data, 2503)}")
--- 2015/Day_14/tests/test_main.py
+++ 2015/Day_14/tests/test_main.py
@@ -5,5 +5,26 @@ Tests for functions in the main script.
 import main
 
 
-def test_1():
-    pass
+def test_parse_data():
+    assert main.parse_data("./data/example_01.txt") == {
+        "Comet": {"fly_speed": 14, "fly_time": 10, "rest_time": 127},
+        "Dancer": {"fly_speed": 16, "fly_time": 11, "rest_time": 162},
+    }
+
+
+def test_distance_comet():
+    assert [
+        x
+        for x in main.dist_trav(
+            {"fly_speed": 14, "fly_time": 10, "rest_time": 127}, 1000
+        )
+    ][-1] == 1120
+
+
+def test_distance_dancer():
+    assert [
+        x
+        for x in main.dist_trav(
+            {"fly_speed": 16, "fly_time": 11, "rest_time": 162}, 1000
+        )
+    ][-1] == 1056
