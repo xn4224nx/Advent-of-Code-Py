d0932ffa125827646ac83cf97c443f5d8410d045
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-08-31 13:13:57 +0100

    Part 2 completed.

--- 2015/Day_20/main.py
+++ 2015/Day_20/main.py
@@ -22,12 +22,20 @@ presents equal to ten times his or her number at each house.
 
 PART 1: What is the lowest house number of the house to get at least as many
         presents as the number in your puzzle input?
+
+The Elves decide they don't want to visit an infinite number of houses. Instead,
+each Elf will stop after delivering presents to 50 houses. To make up for it,
+they decide to deliver presents equal to eleven times their number at each
+house.
+
+PART 2: With these changes, what is the new lowest house number of the house to
+        get at least as many presents as the number in your puzzle input?
 """
 
 import numpy as np
 
 
-def find_lowest_house(num_pres: int) -> int:
+def find_lowest_house(num_pres: int, elf_lim: bool = False) -> int:
     """
     Find the lowest house number to get a certain number of presents.
     """
@@ -38,10 +46,14 @@ def find_lowest_house(num_pres: int) -> int:
 
     # Simulate the elves delivering presents
     for idx in range(1, num_houses):
-        house_pres[idx::idx] += 10 * idx
+        if not elf_lim:
+            house_pres[idx::idx] += 10 * idx
+        else:
+            house_pres[idx : (idx * 50) + 1 : idx] += 11 * idx
 
     return np.where(house_pres >= num_pres)[0][0]
 
 
 if __name__ == "__main__":
     print(f"Part 1 = {find_lowest_house(34000000)}")
+    print(f"Part 1 = {find_lowest_house(34000000, True)}")
