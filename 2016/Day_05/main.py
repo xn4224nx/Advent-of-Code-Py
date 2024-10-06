2b8a2c60ec481db0602edcdddce2c97aeab42333
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-10-06 19:51:29 +0100

    Part 1 completed.

--- 2016/Day_05/main.py
+++ 2016/Day_05/main.py
@@ -15,12 +15,14 @@ hash is the next character of the password.
 PART 1: Given the actual Door ID, what is the password?
 """
 
+import hashlib
+
 
 def hash_door_index(door: str, index: int) -> str:
     """
     Find the hash of the string and a particular index
     """
-    pass
+    return hashlib.md5(f"{door}{index}".encode("utf-8")).hexdigest()
 
 
 def find_password(door_id: str) -> str:
@@ -28,8 +30,24 @@ def find_password(door_id: str) -> str:
     Search the for the 8 character password for a room with a particular
     door id.
     """
-    pass
+    password = ""
+    idx = 0
+
+    # Find the digest for each door id and index
+    while len(password) < 8:
+        digest = hash_door_index(door_id, idx)
+
+        # Has a new character in the password been found ?
+        if digest[:5] == "00000":
+            password += digest[5]
+            print("", end=f"\r{password}")
+
+        # Prepare to test the next index
+        idx += 1
+
+    return password
 
 
 if __name__ == "__main__":
-    pass
+    door_code = find_password("ffykfhsq")
+    print(f"\nPart 1 = '{door_code}'")
--- 2016/Day_05/tests/test_main.py
+++ 2016/Day_05/tests/test_main.py
@@ -10,7 +10,7 @@ def test_hash_door_index_1():
 
 
 def test_next_pass_char_1():
-    assert hash_door_index("abc", 3231929)[6] == "1"
+    assert hash_door_index("abc", 3231929)[5] == "1"
 
 
 def test_hash_door_index_2():
@@ -18,7 +18,7 @@ def test_hash_door_index_2():
 
 
 def test_next_pass_char_2():
-    assert hash_door_index("abc", 5017308)[6] == "8"
+    assert hash_door_index("abc", 5017308)[5] == "8"
 
 
 def test_hash_door_index_3():
@@ -26,7 +26,7 @@ def test_hash_door_index_3():
 
 
 def test_next_pass_char_3():
-    assert hash_door_index("abc", 5278568)[6] == "f"
+    assert hash_door_index("abc", 5278568)[5] == "f"
 
 
 def test_find_password_1():
