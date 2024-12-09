d2f5663d82b607908c135a491019d24b6be1e44a
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-12-09 08:59:51 +0000

    All tests passed.

--- 2016/Day_13/main.py
+++ 2016/Day_13/main.py
@@ -32,27 +32,83 @@ PART 1: What is the fewest number of steps required for you to reach 31,39?
 
 class Maze:
     def __init__(self, favorite_number: int):
-        pass
+        self.fav_num = favorite_number
 
     def is_wall(self, point: tuple[int, int]) -> bool:
         """
         Determine if a point is a wall and cannot be traversed.
         """
-        pass
+        x = point[0]
+        y = point[1]
+
+        # Calcuate a specific value using the point and a favorite number then
+        # count the number of 1s in the binary form of the number.
+        num_ones = (x * x + 3 * x + 2 * x * y + y + y * y + self.fav_num).bit_count()
+
+        # If there are an odd number of ones it is a wall
+        return num_ones % 2 != 0
 
     def poss_moves(self, start_point: tuple[int, int]) -> list[tuple[int, int]]:
         """
         Find, from a starting point all possible next moves and the points the
-        move would take it.
+        move would take it. Diagonal moves are not possible, only up, down, left
+        and right.
         """
-        pass
+        s_x = start_point[0]
+        s_y = start_point[1]
+        pos_moves = []
+
+        # Up
+        up = (s_x, s_y - 1)
+        if s_y != 0 and not self.is_wall(up):
+            pos_moves.append(up)
+
+        # Down
+        dwn = (s_x, s_y + 1)
+        if not self.is_wall(dwn):
+            pos_moves.append(dwn)
+
+        # Left
+        lft = (s_x - 1, s_y)
+        if s_x != 0 and not self.is_wall(lft):
+            pos_moves.append(lft)
+
+        # Right
+        rgt = (s_x + 1, s_y)
+        if not self.is_wall(rgt):
+            pos_moves.append(rgt)
+
+        return pos_moves
 
     def find_min_path_len(self, dest_point: tuple[int, int]) -> int:
         """
         Find the minimum length of the path through the maze to a specific
-        point.
+        point. I assume that no point should be traversed twice.
         """
-        pass
+        curr_pnts = [(1, 1)]
+        seen_pnts = set()
+        total_moves = 0
+
+        # Iterate until the end point is reached
+        while True:
+            nxt_pnts = []
+            total_moves += 1
+
+            # Find the possible next moves and mark the current ones as seen
+            for pnt in curr_pnts:
+                seen_pnts.add(pnt)
+                nxt_pnts += self.poss_moves(pnt)
+
+            if not nxt_pnts:
+                raise Exception("There are no viable next moves!")
+
+            # See if the destination has been reached
+            if dest_point in nxt_pnts:
+                break
+
+            curr_pnts = nxt_pnts
+
+        return total_moves
 
 
 if __name__ == "__main__":
