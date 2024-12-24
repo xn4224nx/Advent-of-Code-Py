41aa75375fcb3e1905b80a633495cb2b91db9f08
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-12-24 10:23:44 +0000

    All tests passed.

--- 2016/Day_17/main.py
+++ 2016/Day_17/main.py
@@ -53,10 +53,15 @@ PART 1: Given your vault's passcode, what is the shortest path (the actual
         path, not just the length) to reach the vault?
 """
 
+import hashlib
+
 
 class Route:
     def __init__(self, seed: str):
         self.seed = seed
+        self.start = (0, 0)
+        self.vault = (3, 3)
+        self.limit = (3, 3)
 
     def hash_md5(self, msg: str) -> str:
         """
@@ -64,21 +69,92 @@ class Route:
         internal salt value. Only return the first four characters of the
         hash.
         """
-        pass
+        return hashlib.md5((self.seed + msg).encode()).hexdigest()[:4]
 
     def detect_next_steps(self, previous_path: str) -> list[str]:
         """
         Determine the next possible moves that could be made in the current
         route.
         """
-        pass
+        valid_chars = ["b", "c", "d", "e", "f"]
+        next_steps = []
+
+        hash_guide = self.hash_md5(previous_path)
+
+        if hash_guide[0] in valid_chars:
+            next_steps.append("U")
+
+        if hash_guide[1] in valid_chars:
+            next_steps.append("D")
+
+        if hash_guide[2] in valid_chars:
+            next_steps.append("L")
+
+        if hash_guide[3] in valid_chars:
+            next_steps.append("R")
+
+        return next_steps
+
+    def path_2_coords(self, path: str) -> (int, int):
+        """
+        Determine the end coordinates of a specific path.
+        """
+        curr_pnt = (self.start[0], self.start[1])
+
+        for move in path:
+            if move == "U":
+                curr_pnt = (curr_pnt[0], curr_pnt[1] - 1)
+            elif move == "D":
+                curr_pnt = (curr_pnt[0], curr_pnt[1] + 1)
+            elif move == "L":
+                curr_pnt = (curr_pnt[0] - 1, curr_pnt[1])
+            elif move == "R":
+                curr_pnt = (curr_pnt[0] + 1, curr_pnt[1])
+            else:
+                raise Exception(f"Unknown move '{move}'!")
+
+        return curr_pnt
 
     def find_shortest_path(self) -> str:
         """
         Find and return the path that will get from the start point to the
         vault in the smallest number of steps.
         """
-        pass
+        viable_paths = {""}
+
+        # Make steps in the maze until the end point is found
+        while True:
+            new_paths = set()
+
+            # Create & validate new paths
+            for path in viable_paths:
+
+                # Work out the next possible steps
+                for step in self.detect_next_steps(path):
+                    n_path = path + step
+                    pnt = self.path_2_coords(n_path)
+
+                    # Check that the path is possible
+                    if (
+                        pnt[0] < 0
+                        or pnt[1] < 0
+                        or pnt[0] > self.limit[0]
+                        or pnt[1] > self.limit[0]
+                    ):
+                        continue
+
+                    # Check for a solution
+                    if pnt == self.vault:
+                        return n_path
+
+                    # Otherwise save it for the next iteration
+                    new_paths.add(n_path)
+
+            # Prepare for the next iteration
+            if not new_paths:
+                raise Exception("No viable path found!")
+            else:
+                viable_paths = new_paths
 
 
 if __name__ == "__main__":
--- 2016/Day_17/tests/test_main.py
+++ 2016/Day_17/tests/test_main.py
@@ -13,10 +13,22 @@ def test_hashing():
     assert test.hash_md5("DU") == "528e"
 
 
+def test_path_2_coords():
+    test = Route("hijkl")
+
+    assert test.path_2_coords("") == test.start
+    assert test.path_2_coords("DDD") == (0, 3)
+    assert test.path_2_coords("RRR") == (3, 0)
+
+    assert test.path_2_coords("DDRRRD") == (3, 3)
+    assert test.path_2_coords("DDUDRLRRUDRD") == (3, 3)
+    assert test.path_2_coords("DRURDRUDDLLDLUURRDULRLDUUDDDRR") == (3, 3)
+
+
 def test_detect_next_steps():
     test = Route("hijkl")
-    assert set(test.detect_next_steps("")) == {"D"}
-    assert set(test.detect_next_steps("D")) == {"U", "R"}
+    assert set(test.detect_next_steps("")) == {"U", "D", "L"}
+    assert set(test.detect_next_steps("D")) == {"U", "L", "R"}
     assert set(test.detect_next_steps("DR")) == set()
     assert set(test.detect_next_steps("DU")) == {"R"}
     assert set(test.detect_next_steps("DUR")) == set()
