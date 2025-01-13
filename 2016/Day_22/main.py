085363ab83de6f818c66ee70bbe62601260c09fc
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-01-13 19:49:18 +0000

    Incomplete part 2.

--- 2016/Day_22/main.py
+++ 2016/Day_22/main.py
@@ -34,18 +34,31 @@ such that:
     -   The data on node A (its Used) would fit on node B (its Avail).
 
 PART 1: How many viable pairs of nodes are there?
+
+Now that you have a better understanding of the grid, it's time to get to work.
+
+Your goal is to gain access to the data which begins in the node with y=0 and
+the highest x (that is, the node in the top-right corner).
+
+PART 2: What is the fewest number of steps required to move your goal data to
+        node-x0-y0?
 """
 
+import sys
 import re
+import numpy as np
 from itertools import permutations
 
 
 class NodeGrid:
     def __init__(self, datafile: str):
-        node_pat = r"(\S+)\s+([0-9]+)T\s+([0-9]+)T\s+([0-9]+)T\s+([0-9]+)%"
+        node_pat = r"x(\d+)\-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)%"
+        grid_max = (0, 0)
+        node_usage = []
+        node_size = []
+        self.node_loc = []
 
-        self.nodes = []
-
+        # Read the raw data from file
         with open(datafile, "r") as fp:
             for idx, line in enumerate(fp):
                 if idx < 2:
@@ -55,42 +68,132 @@ class NodeGrid:
                 matches = re.search(node_pat, line)
 
                 if matches is not None:
-                    self.nodes.append(
-                        {
-                            "Addr": matches.group(1),
-                            "Size": int(matches.group(2)),
-                            "Used": int(matches.group(3)),
-                            "Avail": int(matches.group(4)),
-                        }
-                    )
+                    node_size.append(int(matches.group(3)))
+                    node_usage.append(int(matches.group(4)))
+                    self.node_loc.append((int(matches.group(1)), int(matches.group(2))))
 
-    def viable_pairs(self) -> list[(int, int)]:
+        # Create the 2D node grid as full of partial nodes
+        self.nodes = ["." for x in range(len(self.node_loc))]
+
+        # Find the empty node
+        for empt_idx in range(len(self.node_loc)):
+            if node_usage[empt_idx] == 0:
+                self.nodes[empt_idx] = "_"
+                break
+        else:
+            raise Exception("No empty node found!")
+
+        # Catagorise the full nodes
+        for idx in range(len(self.node_loc)):
+            if node_usage[idx] > node_size[empt_idx]:
+                self.nodes[idx] = "#"
+
+        # Find the goal node
+        max_x = 0
+        max_x_idx = 0
+
+        for idx in range(len(self.node_loc)):
+            if self.node_loc[idx][1] == 0 and self.node_loc[idx][0] > max_x:
+                max_x = self.node_loc[idx][0]
+                max_x_idx = idx
+
+        self.nodes[max_x_idx] = "G"
+
+    def are_nodes_neighbours(self, node_a_idx: int, node_b_idx: int) -> bool:
+        """
+        Determine if two nodes are next to one another.
+        """
+        if node_a_idx == node_b_idx:
+            return False
+
+        a_x, a_y = self.node_loc[node_a_idx]
+        b_x, b_y = self.node_loc[node_b_idx]
+
+        if (a_x == b_x and (a_y - 1 == b_y or a_y + 1 == b_y)) or (
+            a_y == b_y and (a_x - 1 == b_x or a_x + 1 == b_x)
+        ):
+            return True
+        else:
+            return False
+
+    def viable_pairs(
+        self, curr_node_usage: tuple[str], next_door: bool = False
+    ) -> list[(int, int)]:
         """
         Determine the viable node pairs in the grid in its current
         state.
         """
-        node_idxs = [x for x in range(len(self.nodes))]
         pairs = []
 
         # Each permuation of node indexes assess viability
-        for idx_a, idx_b in permutations(node_idxs, 2):
-
-            # Node A is not empty (its Used is not zero).
-            if self.nodes[idx_a]["Used"] == 0:
-                continue
+        for idx_a, idx_b in permutations([x for x in range(len(self.nodes))], 2):
 
             # Nodes A and B are not the same node.
             if idx_a == idx_b:
                 continue
 
-            # The data on node A (its Used) would fit on node B (its Avail).
-            if self.nodes[idx_a]["Used"] > self.nodes[idx_b]["Avail"]:
+            # Ensure there is space for a move
+            if curr_node_usage[idx_b] != "_":
                 continue
 
-            pairs.append((idx_a, idx_b))
+            # Ensure there is data to move
+            if curr_node_usage[idx_a] not in [".", "G"]:
+                continue
+
+            # Test if the nodes are next to each other
+            if next_door and self.are_nodes_neighbours(idx_a, idx_b):
+                pairs.append((idx_a, idx_b))
+            elif not next_door:
+                pairs.append((idx_a, idx_b))
 
         return pairs
 
+    def moves_empty_to_data(self) -> int:
+        """
+        Find the shortest path to move the empty space to the node just left of
+        the cruical data node. Return the minimum moves required to get there
+        and the state of the disk usage in the grid after this move.
+        """
+        unvisted_nodes = set([x for x in range(len(self.nodes))])
+        dist_to_nodes = {x: sys.maxsize for x in range(len(self.nodes))}
+        dest_node = self.nodes.index("G") - 1
+
+        # Start at the empty node
+        dist_to_nodes[self.nodes.index("_")] = 0
+
+        while True:
+
+            # Pick the unvisited node with the smallest distance
+            curr_min = sys.maxsize
+            curr_min_idx = 0
+            for n_idx in unvisted_nodes:
+                if dist_to_nodes[n_idx] < curr_min:
+                    curr_min = dist_to_nodes[n_idx]
+                    curr_min_idx = n_idx
+
+            # Abort the loop if the destination is reached
+            if curr_min_idx == dest_node:
+                break
+
+            # Find all the neighbours of the current minimum and set their dist
+            for n_idx in unvisted_nodes:
+                if self.are_nodes_neighbours(n_idx, curr_min_idx):
+                    dist_to_nodes[n_idx] = curr_min + 1
+
+            unvisted_nodes.remove(curr_min_idx)
+
+        return dist_to_nodes[dest_node]
+
+    def data_extact_min_moves(self) -> int:
+        """
+        Calculate the total number of moves to get the target data 'G' to the
+        access node at 0,0.
+        """
+        moves_for_empty = self.moves_empty_to_data()
+        return moves_for_empty + 5 * (self.node_loc[self.nodes.index("G")][0] - 1) + 1
+
 
 if __name__ == "__main__":
-    print(f"Part 1 = {len(NodeGrid('./data/input.txt').viable_pairs())}")
+    cluster = NodeGrid("./data/input.txt")
+    print(f"Part 1 = {len(cluster.viable_pairs(cluster.nodes))}")
+    print(f"Part 2 = {cluster.data_extact_min_moves()}")
--- 2016/Day_22/tests/test_main.py
+++ 2016/Day_22/tests/test_main.py
@@ -7,22 +7,65 @@ from main import NodeGrid
 
 def test_data_parse():
     test = NodeGrid("./data/example_01.txt")
-    assert test.nodes == [
-        {"Addr": "/dev/grid/node-x0-y19", "Size": 92, "Used": 66, "Avail": 26},
-        {"Addr": "/dev/grid/node-x0-y20", "Size": 94, "Used": 69, "Avail": 25},
-        {"Addr": "/dev/grid/node-x0-y21", "Size": 88, "Used": 65, "Avail": 23},
-        {"Addr": "/dev/grid/node-x0-y22", "Size": 87, "Used": 72, "Avail": 15},
-        {"Addr": "/dev/grid/node-x0-y23", "Size": 92, "Used": 66, "Avail": 26},
-        {"Addr": "/dev/grid/node-x0-y24", "Size": 89, "Used": 72, "Avail": 17},
-        {"Addr": "/dev/grid/node-x1-y0", "Size": 86, "Used": 66, "Avail": 20},
-        {"Addr": "/dev/grid/node-x1-y1", "Size": 93, "Used": 64, "Avail": 29},
-        {"Addr": "/dev/grid/node-x1-y2", "Size": 92, "Used": 65, "Avail": 27},
-        {"Addr": "/dev/grid/node-x1-y3", "Size": 92, "Used": 70, "Avail": 22},
-        {"Addr": "/dev/grid/node-x1-y4", "Size": 87, "Used": 72, "Avail": 15},
-        {"Addr": "/dev/grid/node-x1-y5", "Size": 87, "Used": 69, "Avail": 18},
-        {"Addr": "/dev/grid/node-x7-y17", "Size": 92, "Used": 0, "Avail": 92},
+
+    assert test.nodes == [".", ".", "#", ".", "_", ".", "G", ".", "."]
+    assert test.node_loc == [
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (1, 0),
+        (1, 1),
+        (1, 2),
+        (2, 0),
+        (2, 1),
+        (2, 2),
     ]
 
 
 def test_find_viable_pairs():
-    assert len(NodeGrid("./data/example_01.txt").viable_pairs()) == 12
+    test = NodeGrid("./data/example_01.txt")
+    assert len(test.viable_pairs(test.nodes)) == 7
+
+
+def test_find_ajacent_pairs():
+    test = NodeGrid("./data/example_01.txt")
+    assert test.viable_pairs((".", ".", ".", ".", "_", ".", ".", ".", "."), True) == [
+        (1, 4),
+        (3, 4),
+        (5, 4),
+        (7, 4),
+    ]
+
+    assert test.viable_pairs(
+        (
+            "_",
+            ".",
+            ".",
+            ".",
+            ".",
+            ".",
+            ".",
+            ".",
+            ".",
+        ),
+        True,
+    ) == [
+        (1, 0),
+        (3, 0),
+    ]
+
+    assert test.viable_pairs((".", ".", ".", ".", ".", "_", ".", ".", "."), True) == [
+        (2, 5),
+        (4, 5),
+        (8, 5),
+    ]
+
+
+def test_moves_empty_to_data():
+    assert NodeGrid("./data/example_01.txt").moves_empty_to_data() == 1
+    assert NodeGrid("./data/example_02.txt").moves_empty_to_data() == 69
+
+
+def test_minimum_data_moves():
+    assert NodeGrid("./data/example_01.txt").data_extact_min_moves() == 7
+    assert NodeGrid("./data/example_02.txt").data_extact_min_moves() == 225
