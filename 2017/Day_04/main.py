0e7f66b434d108bf5b3b1f3081a1b3be9d26494a
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-02-17 14:39:58 +0000

    Part 1 completed.

--- 2017/Day_04/main.py
+++ 2017/Day_04/main.py
@@ -15,6 +15,28 @@ For example:
 
 PART 1: The system's full passphrase list is available as your puzzle input. How
         many passphrases are valid?
+
+For added security, yet another system policy has been put in place. Now, a
+valid passphrase must contain no two words that are anagrams of each other -
+that is, a passphrase is invalid if any word's letters can be rearranged to form
+any other word in the passphrase.
+
+For example:
+
+    -   abcde fghij is a valid passphrase.
+
+    -   abcde xyz ecdab is not valid - the letters from the third word can be
+        rearranged to form the first word.
+
+    -   a ab abc abd abf abj is a valid passphrase, because all letters need to
+        be used when forming another word.
+
+    -   iiii oiii ooii oooi oooo is valid.
+
+    -   oiii ioii iioi iiio is not valid - any of these words can be rearranged
+        to form any other word.
+
+PART 2: Under this new system policy, how many passphrases are valid?
 """
 
 
@@ -26,24 +48,32 @@ class SecuritySystem:
             for line in fp.readlines():
                 self.all_passphrases.append(line.strip())
 
-    def valid_phrase_dupe(self, passphrase: str) -> bool:
+    def valid_phrase(self, passphrase: str, anagram: bool = False) -> bool:
         """
         Extract the words in a passphrase and determine if there are any
         duplicates, if so the passphrase is not valid.
         """
         words = passphrase.split()
+
+        # If anagrams are excluded sort the letters in the word so they will be
+        # marked as duplicates when added to a set
+        if anagram:
+            words = ["".join(sorted(x)) for x in words]
+
+        # Check for any duplicate words in the passphrase by comparing the set
         num_words = len(words)
         unique_words = len(set(words))
 
         return num_words == unique_words
 
-    def count_valid_passphrases(self) -> int:
+    def count_valid_passphrases(self, anagram: bool = False) -> int:
         """
         Count the number of passphrases that contain no duplicate words.
         """
-        return sum([self.valid_phrase_dupe(x) for x in self.all_passphrases])
+        return sum([self.valid_phrase(x, anagram) for x in self.all_passphrases])
 
 
 if __name__ == "__main__":
     sec_sys = SecuritySystem("./data/input.txt")
     print(f"Part 1 = {sec_sys.count_valid_passphrases()}")
+    print(f"Part 1 = {sec_sys.count_valid_passphrases(True)}")
--- 2017/Day_04/tests/test_main.py
+++ 2017/Day_04/tests/test_main.py
@@ -5,7 +5,7 @@ Tests for the main script
 from main import SecuritySystem
 
 
-def test_data_ingestion():
+def test_data_ingestion_exp1():
     assert SecuritySystem("./data/example_01.txt").all_passphrases == [
         "aa bb cc dd ee",
         "aa bb cc dd aa",
@@ -13,24 +13,72 @@ def test_data_ingestion():
     ]
 
 
+def test_data_ingestion_exp2():
+    assert SecuritySystem("./data/example_02.txt").all_passphrases == [
+        "abcde fghij",
+        "abcde xyz ecdab",
+        "a ab abc abd abf abj",
+        "iiii oiii ooii oooi oooo",
+        "oiii ioii iioi iiio",
+    ]
+
+
 def test_dupe_valid_phrase_exp1():
     assert (
-        SecuritySystem("./data/example_01.txt").valid_phrase_dupe("aa bb cc dd ee")
+        SecuritySystem("./data/example_01.txt").valid_phrase("aa bb cc dd ee") == True
+    )
+
+
+def test_dupe_valid_phrase_exp2():
+    assert (
+        SecuritySystem("./data/example_01.txt").valid_phrase("aa bb cc dd aa") == False
+    )
+
+
+def test_dupe_valid_phrase_exp3():
+    assert (
+        SecuritySystem("./data/example_01.txt").valid_phrase("aa bb cc dd aaa") == True
+    )
+
+
+def test_dupe_valid_phrase_exp4():
+    assert (
+        SecuritySystem("./data/example_02.txt").valid_phrase("abcde fghij", True)
         == True
     )
 
 
-def test_dupe_valid_phrase_exp1():
+def test_dupe_valid_phrase_exp5():
     assert (
-        SecuritySystem("./data/example_01.txt").valid_phrase_dupe("aa bb cc dd aa")
+        SecuritySystem("./data/example_02.txt").valid_phrase("abcde xyz ecdab", True)
         == False
     )
 
 
-def test_dupe_valid_phrase_exp1():
+def test_dupe_valid_phrase_exp6():
     assert (
-        SecuritySystem("./data/example_01.txt").valid_phrase_dupe("aa bb cc dd aaa")
+        SecuritySystem("./data/example_02.txt").valid_phrase(
+            "a ab abc abd abf abj", True
+        )
         == True
+    )
+
+
+def test_dupe_valid_phrase_exp7():
+    assert (
+        SecuritySystem("./data/example_02.txt").valid_phrase(
+            "iiii oiii ooii oooi oooo", True
+        )
+        == True
+    )
+
+
+def test_dupe_valid_phrase_exp8():
+    assert (
+        SecuritySystem("./data/example_02.txt").valid_phrase(
+            "oiii ioii iioi iiio", True
+        )
+        == False
     )
 
 
