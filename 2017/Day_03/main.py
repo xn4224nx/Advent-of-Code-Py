4347f256201893f37e34e2992bf3b8421e450e0f
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-02-17 13:39:47 +0000

    Part 2 completed.

--- 2017/Day_03/main.py
+++ 2017/Day_03/main.py
@@ -29,33 +29,152 @@ For example:
 
 PART 1: How many steps are required to carry the data from the square identified
         in your puzzle input all the way to the access port?
+
+As a stress test on the system, the programs here clear the grid and then store
+the value 1 in square 1. Then, in the same allocation order as shown above, they
+store the sum of the values in all adjacent squares, including diagonals.
+
+So, the first few squares' values are chosen as follows:
+
+    -   Square 1 starts with the value 1.
+
+    -   Square 2 has only one adjacent filled square (with value 1), so it also
+        stores 1.
+
+    -   Square 3 has both of the above squares as neighbors and stores the sum
+        of their values, 2.
+
+    -   Square 4 has all three of the aforementioned squares as neighbors and
+        stores the sum of their values, 4.
+
+    -   Square 5 only has the first and fourth squares as neighbors, so it gets
+        the value 5.
+
+Once a square is written, its value does not change. Therefore, the first few
+squares would receive the following values:
+
+    147  142  133  122   59
+    304    5    4    2   57
+    330   10    1    1   54
+    351   11   23   25   26
+    362  747  806--->   ...
+
+PART 2: What is the first value written that is larger than your puzzle input?
 """
+
+import numpy as np
+import math
 
 
 class SpiralMemory:
-    def __init__(self, max_val: int):
+    def __init__(self, max_val: int = 0):
         self.max_val = max_val
 
-    def moves_to_exit(self) -> int:
+    def coord_of_step(self, step_idx: int) -> (int, int):
+        """
+        Find the coordinates of a step in the sprial assuming the origin is
+        point (0,0).
+        """
+        # Calculate the ring characteristics
+        ring = int(-(-(step_idx**0.5) // 1) // 2)
+        ring_side_len = int(2 * ring + 1)
+        ring_final_val = int((ring_side_len) ** 2)
+
+        # Calculate the value at each of the corners of the ring
+        bot_left = ring_final_val - (ring_side_len - 1)
+        top_left = ring_final_val - 2 * (ring_side_len - 1)
+        top_rght = ring_final_val - 3 * (ring_side_len - 1)
+
+        # Using the above infomation calculate the coordinates of the point
+
+        # Test to see if this step is on a corner
+        if step_idx == ring_final_val:
+            pnt = (ring, -ring)
+
+        elif step_idx == bot_left:
+            pnt = (-ring, -ring)
+
+        elif step_idx == top_left:
+            pnt = (-ring, ring)
+
+        elif step_idx == top_rght:
+            pnt = (ring, ring)
+
+        # Bottow row points
+        elif ring_final_val > step_idx > bot_left:
+            pnt = (step_idx - bot_left - ring_side_len // 2, -ring)
+
+        # Top row points
+        elif top_left > step_idx > top_rght:
+            pnt = ((top_rght + ring_side_len // 2) - step_idx, ring)
+
+        # Left column points
+        elif bot_left > step_idx > top_left:
+            pnt = (-ring, (top_left + ring_side_len // 2) - step_idx)
+
+        # Right column points
+        elif top_rght > step_idx:
+            pnt = (ring, step_idx - top_rght + ring_side_len // 2)
+
+        else:
+            raise Exception(f"Step {step_idx} could not be placed at a point.")
+
+        return (int(pnt[0]), int(pnt[1]))
+
+    def moves_to_exit(self, step_idx: int) -> int:
         """
         Calculate the manhattan distance that a square at `max_val` would need
         to travel to get to the exit square at one.
         """
-        if self.max_val in [0, 1]:
-            return self.max_val
 
-        # What ring in the spiral is the number located?
-        ring = -(-(self.max_val**0.5) // 1) // 2
+        # What are the coordinates of this step?
+        coord = self.coord_of_step(step_idx)
 
-        # What is the remaining steps to take from the start point in the ring?
-        rem_steps = self.max_val - ((2 * ring - 1) ** 2) % (4 * (2 * ring - 1) + 4)
+        # Return the sum of the magnitudes of the x and y values.
+        return int(abs(coord[0]) + abs(coord[1]))
 
-        # What is the offset from the centre of each ring side
-        offset = abs(((rem_steps % (2 * ring))) - ring)
+    def find_first_gt_max_val(self) -> int:
+        """
+        Assuming the squares start at one and have their value decided by the
+        sum of already created adjacent squares. What is the first value
+        created larger than the max value? This corresponds to the OEIS sequence
+        A141481. https://oeis.org/A141481
+        """
+        step_idx = 2
+        pnt_vals = {(0, 0): 1}
 
-        # What is the manhattan distance to the centre?
-        return int(offset + ring)
+        # Generate the values for each step
+        while True:
+            pnt = self.coord_of_step(step_idx)
+
+            # Define the possible adjacent points to the current one
+            possible_adj_pnts = [
+                (pnt[0] + 1, pnt[1] + 1),
+                (pnt[0], pnt[1] + 1),
+                (pnt[0] - 1, pnt[1] + 1),
+                (pnt[0] + 1, pnt[1]),
+                (pnt[0] - 1, pnt[1]),
+                (pnt[0] + 1, pnt[1] - 1),
+                (pnt[0], pnt[1] - 1),
+                (pnt[0] - 1, pnt[1] - 1),
+            ]
+
+            # Calculate the value for this point
+            pnt_sum = 0
+            for adj_pnt in possible_adj_pnts:
+                if adj_pnt in pnt_vals:
+                    pnt_sum += pnt_vals[adj_pnt]
+
+            # Check for a solution
+            if pnt_sum > self.max_val:
+                return pnt_sum
+
+            else:
+                pnt_vals[pnt] = pnt_sum
+                step_idx += 1
 
 
 if __name__ == "__main__":
-    print(f"Part 1 = {SpiralMemory(325489).moves_to_exit()}")
+    strg = SpiralMemory(325489)
+    print(f"Part 1 = {strg.moves_to_exit(strg.max_val)}")
+    print(f"Part 2 = {strg.find_first_gt_max_val()}")
--- 2017/Day_03/tests/test_main.py
+++ 2017/Day_03/tests/test_main.py
@@ -5,21 +5,113 @@ Tests for the main script
 from main import SpiralMemory
 
 
+def test_coord_of_step_exp01():
+    assert SpiralMemory().coord_of_step(1) == (0, 0)
+
+
+def test_coord_of_step_exp02():
+    assert SpiralMemory().coord_of_step(2) == (1, 0)
+
+
+def test_coord_of_step_exp03():
+    assert SpiralMemory().coord_of_step(3) == (1, 1)
+
+
+def test_coord_of_step_exp04():
+    assert SpiralMemory().coord_of_step(4) == (0, 1)
+
+
+def test_coord_of_step_exp05():
+    assert SpiralMemory().coord_of_step(5) == (-1, 1)
+
+
+def test_coord_of_step_exp06():
+    assert SpiralMemory().coord_of_step(7) == (-1, -1)
+
+
+def test_coord_of_step_exp07():
+    assert SpiralMemory().coord_of_step(8) == (0, -1)
+
+
+def test_coord_of_step_exp08():
+    assert SpiralMemory().coord_of_step(9) == (1, -1)
+
+
+def test_coord_of_step_exp09():
+    assert SpiralMemory().coord_of_step(13) == (2, 2)
+
+
+def test_coord_of_step_exp10():
+    assert SpiralMemory().coord_of_step(10) == (2, -1)
+
+
+def test_coord_of_step_exp11():
+    assert SpiralMemory().coord_of_step(25) == (2, -2)
+
+
+def test_coord_of_step_exp12():
+    assert SpiralMemory().coord_of_step(14) == (1, 2)
+
+
+def test_coord_of_step_exp13():
+    assert SpiralMemory().coord_of_step(15) == (0, 2)
+
+
+def test_coord_of_step_exp14():
+    assert SpiralMemory().coord_of_step(20) == (-2, -1)
+
+
+def test_coord_of_step_exp15():
+    assert SpiralMemory().coord_of_step(22) == (-1, -2)
+
+
+def test_coord_of_step_exp16():
+    assert SpiralMemory().coord_of_step(24) == (1, -2)
+
+
 def test_exit_moves_exp1():
-    assert SpiralMemory(1).moves_to_exit() == 1
+    assert SpiralMemory().moves_to_exit(2) == 1
 
 
 def test_exit_moves_exp2():
-    assert SpiralMemory(12).moves_to_exit() == 3
+    assert SpiralMemory().moves_to_exit(12) == 3
 
 
 def test_exit_moves_exp3():
-    assert SpiralMemory(23).moves_to_exit() == 2
+    assert SpiralMemory().moves_to_exit(23) == 2
 
 
 def test_exit_moves_exp4():
-    assert SpiralMemory(1024).moves_to_exit() == 31
+    assert SpiralMemory().moves_to_exit(1024) == 31
 
 
 def test_exit_moves_exp5():
-    assert SpiralMemory(0).moves_to_exit() == 0
+    assert SpiralMemory().moves_to_exit(1) == 0
+
+
+def test_find_first_gt_max_val_exp1():
+    assert SpiralMemory(800).find_first_gt_max_val() == 806
+
+
+def test_find_first_gt_max_val_exp2():
+    assert SpiralMemory(700).find_first_gt_max_val() == 747
+
+
+def test_find_first_gt_max_val_exp3():
+    assert SpiralMemory(300).find_first_gt_max_val() == 304
+
+
+def test_find_first_gt_max_val_exp4():
+    assert SpiralMemory(20).find_first_gt_max_val() == 23
+
+
+def test_find_first_gt_max_val_exp5():
+    assert SpiralMemory(55).find_first_gt_max_val() == 57
+
+
+def test_find_first_gt_max_val_exp6():
+    assert SpiralMemory(133).find_first_gt_max_val() == 142
+
+
+def test_find_first_gt_max_val_exp6():
+    assert SpiralMemory(5).find_first_gt_max_val() == 10
