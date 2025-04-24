36dac641080cdfeef1b3cf0737e559bbcb425774
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-04-25 00:49:04 +0100

    Part 2 completed.

--- 2017/Day_18/main.py
+++ 2017/Day_18/main.py
@@ -78,24 +78,78 @@ played is 4.
 PART 1: What is the value of the recovered frequency (the value of the most
         recently played sound) the first time a rcv instruction is executed
         with a non-zero value?
+
+As you congratulate yourself for a job well done, you notice that the
+documentation has been on the back of the tablet this entire time. While you
+actually got most of the instructions correct, there are a few key differences.
+This assembly code isn't about sound at all - it's meant to be run twice at the
+same time.
+
+Each running copy of the program has its own set of registers and follows the
+code independently - in fact, the programs don't even necessarily run at the
+same speed. To coordinate, they use the send (snd) and receive (rcv)
+instructions:
+
+        -   snd X sends the value of X to the other program. These values wait
+            in a queue until that program is ready to receive them. Each program
+            has its own message queue, so a program can never receive a message
+            it sent.
+
+        -   rcv X receives the next value and stores it in register X. If no
+            values are in the queue, the program waits for a value to be sent to
+            it. Programs do not continue to the next instruction until they have
+            received a value. Values are received in the order they are sent.
+
+Each program also has its own program ID (one 0 and the other 1); the register p
+should begin with this value.
+
+For example:
+
+    snd 1
+    snd 2
+    snd p
+    rcv a
+    rcv b
+    rcv c
+    rcv d
+
+Both programs begin by sending three values to the other. Program 0 sends 1, 2,
+0; program 1 sends 1, 2, 1. Then, each program receives a value (both 1) and
+stores it in a, receives another value (both 2) and stores it in b, and then
+each receives the program ID of the other program (program 0 receives 1; program
+1 receives 0) and stores it in c. Each program now sees a different value in its
+own copy of register c.
+
+Finally, both programs try to rcv a fourth time, but no data is waiting for
+either of them, and they reach a deadlock. When this happens, both programs
+terminate.
+
+It should be noted that it would be equally valid for the programs to run at
+different speeds; for example, program 0 might have sent all three values and
+then stopped at the first rcv before program 1 executed even its first
+instruction.
+
+PART 2: Once both of your programs have terminated (regardless of what caused
+        them to do so), how many times did program 1 send a value?
 """
 
 import re
 
 
 class Duo:
-    def __init__(self, datafile: str):
-        cmd_re = r"([a-z]{3}) (-?[0-9]+|[a-z])\s?(-?[0-9]+|[a-z])?"
-
-        self.idx = 0
-        self.played = []
-        self.received = []
+    def __init__(self, datafile: str, twin_program: bool = False):
+        self.twin_program = twin_program
+        self.regs = [{}, {}]
         self.cmds = []
-        self.reg = {}
+        self.transit = [[], []]
+        self.active_prog = 0
+        self.idx = [0, 0]
+        self.sent_counts = [0, 0]
 
+        # Collect the instructions and the possible registers
         with open(datafile, "r") as fp:
             for line in fp.readlines():
-                raw = re.match(cmd_re, line)
+                raw = re.match(r"([a-z]{3}) (-?[0-9]+|[a-z])\s?(-?[0-9]+|[a-z])?", line)
 
                 tmp_cmd = {"cmd": raw[1]}
 
@@ -104,7 +158,8 @@ class Duo:
                     tmp_cmd["x"] = int(raw[2])
                 else:
                     tmp_cmd["x"] = raw[2]
-                    self.reg[raw[2]] = 0
+                    self.regs[0][raw[2]] = 0
+                    self.regs[1][raw[2]] = 0
 
                 # If the second variable exists add it in
                 if raw[3] is not None:
@@ -112,9 +167,14 @@ class Duo:
                         tmp_cmd["y"] = int(raw[3])
                     else:
                         tmp_cmd["y"] = raw[3]
-                        self.reg[raw[3]] = 0
+                        self.regs[0][raw[3]] = 0
+                        self.regs[1][raw[3]] = 0
 
                 self.cmds.append(tmp_cmd)
+
+        if self.twin_program:
+            self.regs[0]["p"] = 0
+            self.regs[1]["p"] = 1
 
     def parse_value(self, val: int | str) -> int:
         """
@@ -122,38 +182,53 @@ class Duo:
         a value. The register's value or the original value.
         """
         if isinstance(val, str):
-            return self.reg[val]
+            return self.regs[self.active_prog][val]
         else:
             return val
 
+    def non_active_prog(self) -> int:
+        """
+        Determine the currently non-active program.
+        """
+        return (self.active_prog + 1) % len(self.regs)
+
     def snd(self, x_val: int | str):
         """
-        Plays a sound with a frequency equal to the value of x_val.
+        Plays a sound with a frequency equal to the value of x_val if this is
+        not a twin_program.
+
+        If this is a twin program send a value to the non-active programs
+        transit list.
         """
-        self.played.append(self.parse_value(x_val))
-        self.idx += 1
+        if not self.twin_program:
+            self.transit[0].append(self.parse_value(x_val))
+        else:
+            self.transit[self.non_active_prog()].append(self.parse_value(x_val))
+            self.sent_counts[self.active_prog] += 1
+
+        self.idx[self.active_prog] += 1
 
     def set_val(self, x_reg: str, y_val: int | str):
         """
         Sets register x_val to the value of y_val.
         """
-        self.reg[x_reg] = self.parse_value(y_val)
-        self.idx += 1
+        self.regs[self.active_prog][x_reg] = self.parse_value(y_val)
+        self.idx[self.active_prog] += 1
 
     def add(self, x_reg: str, y_val: int | str):
         """
         Increases register x_val by the value of y_val.
         """
-        self.reg[x_reg] += self.parse_value(y_val)
-        self.idx += 1
+        self.regs[self.active_prog][x_reg] += self.parse_value(y_val)
+        self.idx[self.active_prog] += 1
 
     def mul(self, x_reg: str, y_val: int | str):
         """
         Sets register x_reg to the result of multiplying the value contained
         in register x_reg by the value of y_val.
         """
-        self.reg[x_reg] *= self.parse_value(y_val)
-        self.idx += 1
+        self.regs[self.active_prog][x_reg] *= self.parse_value(y_val)
+        self.idx[self.active_prog] += 1
 
     def mod(self, x_reg: str, y_val: int | str):
         """
@@ -161,17 +236,27 @@ class Duo:
         in register x_reg by the value of Y (that is, it sets x_reg to the
         result of x_reg modulo Y).
         """
-        self.reg[x_reg] %= self.parse_value(y_val)
-        self.idx += 1
+        self.regs[self.active_prog][x_reg] %= self.parse_value(y_val)
+        self.idx[self.active_prog] += 1
 
     def rcv(self, x_val: int | str):
         """
         Recovers the frequency of the last sound played, but only when the
         value of x_val is not zero. (If it is zero, the command does nothing.)
+        if this is not a twin_program.
+
+        Otherwise if this is a twin-program wait for a value to be sent to this
+        program.
         """
-        if self.parse_value(x_val) != 0:
-            self.received.append(self.played[-1])
-        self.idx += 1
+        if not self.twin_program:
+            if self.parse_value(x_val) != 0:
+                self.transit[1].append(self.transit[0][-1])
+            self.idx[self.active_prog] += 1
+
+        # Extract the last value from the programs transit and set a register
+        elif len(self.transit[self.active_prog]) > 0:
+            self.regs[self.active_prog][x_val] = self.transit[self.active_prog].pop(0)
+            self.idx[self.active_prog] += 1
 
     def jgz(self, x_val: int | str, y_val: int | str):
         """
@@ -181,9 +266,9 @@ class Duo:
         on.)
         """
         if self.parse_value(x_val) > 0:
-            self.idx += self.parse_value(y_val)
+            self.idx[self.active_prog] += self.parse_value(y_val)
         else:
-            self.idx += 1
+            self.idx[self.active_prog] += 1
 
     def execute_cmd(self, cmd_info: dict[str:str]):
         """
@@ -220,11 +305,34 @@ class Duo:
         Find the first recovered frequency when a non-zero rcv command is
         executed.
         """
-        while len(self.received) < 1:
-            self.execute_cmd(self.cmds[self.idx])
+        while len(self.transit[1]) < 1:
+            self.execute_cmd(self.cmds[self.idx[self.active_prog]])
 
-        return self.received[-1]
+        return self.transit[1][-1]
+
+    def sent_values(self, prog_id: int) -> int:
+        """
+        After both programs have terminated count the number of times the
+        specified program sent a value.
+        """
+        assert self.twin_program
+        while True:
+
+            # Run the current command for both programs
+            for prog_id in [0, 1]:
+                self.active_prog = prog_id
+                self.execute_cmd(self.cmds[self.idx[self.active_prog]])
+
+            # If both programs are waiting for a recieve with nothing coming
+            if (
+                self.cmds[self.idx[0]]["cmd"] == "rcv"
+                and self.cmds[self.idx[1]]["cmd"] == "rcv"
+                and len(self.transit[0]) == 0
+                and len(self.transit[0]) == 0
+            ):
+                return self.sent_counts[prog_id]
 
 
 if __name__ == "__main__":
     print(f"Part 1 = {Duo("./data/input.txt").first_rcv_execution()}")
+    print(f"Part 2 = {Duo("./data/input.txt", True).sent_values(1)}")
--- 2017/Day_18/tests/test_main.py
+++ 2017/Day_18/tests/test_main.py
@@ -7,7 +7,7 @@ from main import Duo
 
 def test_parse_data():
     test = Duo("./data/example_01.txt")
-    assert test.idx == 0
+    assert test.idx == [0, 0]
     assert test.cmds == [
         {"cmd": "set", "x": "a", "y": 1},
         {"cmd": "add", "x": "a", "y": 2},
@@ -20,225 +20,232 @@ def test_parse_data():
         {"cmd": "set", "x": "a", "y": 1},
         {"cmd": "jgz", "x": "a", "y": -2},
     ]
-    assert test.reg == {"a": 0}
-    assert test.played == []
-    assert test.received == []
+    assert test.regs == [{"a": 0}, {"a": 0}]
+    assert test.transit == [[], []]
 
 
 def test_send_example_01():
     test = Duo("./data/example_01.txt")
     test.snd(4)
-    assert test.played == [4]
-    assert test.idx == 1
+    assert test.transit[0] == [4]
+    assert test.idx[0] == 1
 
 
 def test_send_example_02():
     test = Duo("./data/example_01.txt")
     test.snd("a")
-    assert test.played == [0]
-    assert test.idx == 1
+    assert test.transit[0] == [0]
+    assert test.idx[0] == 1
 
 
 def test_set_example_01():
     test = Duo("./data/example_01.txt")
     test.set_val("a", 4)
-    assert test.reg == {"a": 4}
-    assert test.idx == 1
+    assert test.regs[0] == {"a": 4}
+    assert test.idx[0] == 1
 
 
 def test_set_example_02():
     test = Duo("./data/example_01.txt")
-    test.reg == {"a": 4, "b": -1}
+    test.regs[0] == {"a": 4, "b": -1}
     test.set_val("b", 4)
-    assert test.reg["b"] == 4
-    assert test.idx == 1
+    assert test.regs[0]["b"] == 4
+    assert test.idx[0] == 1
 
 
 def test_set_example_03():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 2, "b": 0}
+    test.regs[0] = {"a": 2, "b": 0}
     test.set_val("b", "a")
-    assert test.reg["b"] == 2
-    assert test.idx == 1
+    assert test.regs[0]["b"] == 2
+    assert test.idx[0] == 1
 
 
 def test_add_example_01():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 2, "b": 3}
+    test.regs[0] = {"a": 2, "b": 3}
     test.add("a", "b")
-    assert test.reg["a"] == 5
-    assert test.idx == 1
+    assert test.regs[0]["a"] == 5
+    assert test.idx[0] == 1
 
 
 def test_add_example_02():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 2, "b": 3}
+    test.regs[0] = {"a": 2, "b": 3}
     test.add("b", "a")
-    assert test.reg["b"] == 5
-    assert test.idx == 1
+    assert test.regs[0]["b"] == 5
+    assert test.idx[0] == 1
 
 
 def test_add_example_03():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 2, "b": 3}
+    test.regs[0] = {"a": 2, "b": 3}
     test.add("b", 7)
-    assert test.reg["b"] == 10
-    assert test.idx == 1
+    assert test.regs[0]["b"] == 10
+    assert test.idx[0] == 1
 
 
 def test_mul_example_01():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 2, "b": 3}
+    test.regs[0] = {"a": 2, "b": 3}
     test.mul("a", "b")
-    assert test.reg == {"a": 6, "b": 3}
-    assert test.idx == 1
+    assert test.regs[0] == {"a": 6, "b": 3}
+    assert test.idx[0] == 1
 
 
 def test_mul_example_02():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 2, "b": 3}
+    test.regs[0] = {"a": 2, "b": 3}
     test.mul("b", 7)
-    assert test.reg == {"a": 2, "b": 21}
-    assert test.idx == 1
+    assert test.regs[0] == {"a": 2, "b": 21}
+    assert test.idx[0] == 1
 
 
 def test_mod_example_01():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 200, "b": 35}
+    test.regs[0] = {"a": 200, "b": 35}
     test.mod("b", 7)
-    assert test.reg == {"a": 200, "b": 0}
-    assert test.idx == 1
+    assert test.regs[0] == {"a": 200, "b": 0}
+    assert test.idx[0] == 1
 
 
 def test_mod_example_02():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 200, "b": 35}
+    test.regs[0] = {"a": 200, "b": 35}
     test.mod("a", "b")
-    assert test.reg == {"a": 25, "b": 35}
-    assert test.idx == 1
+    assert test.regs[0] == {"a": 25, "b": 35}
+    assert test.idx[0] == 1
 
 
 def test_rcv_example_01():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35}
-    test.played = [11]
+    test.regs[0] = {"a": 0, "b": 35}
+    test.transit[0] = [11]
     test.rcv("a")
-    assert test.received == []
-    assert test.idx == 1
+    assert test.transit[1] == []
+    assert test.idx[0] == 1
 
 
 def test_rcv_example_02():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35}
-    test.played = [112]
+    test.regs[0] = {"a": 0, "b": 35}
+    test.transit[0] = [112]
     test.rcv(0)
-    assert test.received == []
-    assert test.idx == 1
+    assert test.transit[1] == []
+    assert test.idx[0] == 1
 
 
 def test_rcv_example_03():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35}
-    test.played = [-7]
+    test.regs[0] = {"a": 0, "b": 35}
+    test.transit[0] = [-7]
     test.rcv("b")
-    assert test.received == [-7]
-    assert test.idx == 1
+    assert test.transit[1] == [-7]
+    assert test.idx[0] == 1
 
 
 def test_rcv_example_04():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35}
-    test.played = [172]
+    test.regs[0] = {"a": 0, "b": 35}
+    test.transit[0] = [172]
     test.rcv(3)
-    assert test.received == [172]
-    assert test.idx == 1
+    assert test.transit[1] == [172]
+    assert test.idx[0] == 1
 
 
 def test_jgz_example_01():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35, "c": -1}
+    test.regs[0] = {"a": 0, "b": 35, "c": -1}
     test.jgz("a", 4)
-    assert test.idx == 1
+    assert test.idx[0] == 1
 
 
 def test_jgz_example_02():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35, "c": -1}
+    test.regs[0] = {"a": 0, "b": 35, "c": -1}
     test.jgz("b", 4)
-    assert test.idx == 4
+    assert test.idx[0] == 4
 
 
 def test_jgz_example_03():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35, "c": -1}
+    test.regs[0] = {"a": 0, "b": 35, "c": -1}
     test.jgz("c", 4)
-    assert test.idx == 1
+    assert test.idx[0] == 1
 
 
 def test_jgz_example_04():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35, "c": -1}
+    test.regs[0] = {"a": 0, "b": 35, "c": -1}
     test.jgz(0, 4)
-    assert test.idx == 1
+    assert test.idx[0] == 1
 
 
 def test_jgz_example_05():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35, "c": -1}
+    test.regs[0] = {"a": 0, "b": 35, "c": -1}
     test.jgz(12, 4)
-    assert test.idx == 4
+    assert test.idx[0] == 4
 
 
 def test_jgz_example_06():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 0, "b": 35, "c": -1}
+    test.regs[0] = {"a": 0, "b": 35, "c": -1}
     test.jgz(-3, 4)
-    assert test.idx == 1
+    assert test.idx[0] == 1
 
 
 def test_execute_cmd_exp01():
     test = Duo("./data/example_01.txt")
     test.execute_cmd({"cmd": "set", "x": "a", "y": 1})
-    assert test.reg == {"a": 1}
+    assert test.regs[0] == {"a": 1}
 
 
 def test_execute_cmd_exp02():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 1}
+    test.regs[0] = {"a": 1}
     test.execute_cmd({"cmd": "add", "x": "a", "y": 2})
-    assert test.reg == {"a": 3}
+    assert test.regs[0] == {"a": 3}
 
 
 def test_execute_cmd_exp03():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 3}
+    test.regs[0] = {"a": 3}
     test.execute_cmd({"cmd": "mul", "x": "a", "y": "a"})
-    assert test.reg == {"a": 9}
+    assert test.regs[0] == {"a": 9}
 
 
 def test_execute_cmd_exp04():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 9}
+    test.regs[0] = {"a": 9}
     test.execute_cmd({"cmd": "mod", "x": "a", "y": 5})
-    assert test.reg == {"a": 4}
+    assert test.regs[0] == {"a": 4}
 
 
 def test_execute_cmd_exp05():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 4}
+    test.regs[0] = {"a": 4}
     test.execute_cmd({"cmd": "snd", "x": "a"})
-    assert test.reg == {"a": 4}
-    assert test.played == [4]
+    assert test.regs[0] == {"a": 4}
+    assert test.transit[0] == [4]
 
 
 def test_execute_cmd_exp06():
     test = Duo("./data/example_01.txt")
-    test.reg = {"a": 4}
+    test.regs[0] = {"a": 4}
     test.execute_cmd(
         {"cmd": "jgz", "x": "a", "y": -2},
     )
-    assert test.idx == -2
+    assert test.idx[0] == -2
 
 
 def test_first_rcv_execution_exp01():
     assert Duo("./data/example_01.txt").first_rcv_execution() == 4
+
+
+def count_sent_values_exp01():
+    assert Duo("./data/example_02.txt", True).sent_values(0) == 3
+
+
+def count_sent_values_exp02():
+    assert Duo("./data/example_02.txt", True).sent_values(1) == 3
