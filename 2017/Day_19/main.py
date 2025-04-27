03975e81e65ab81f1ecaa5a398da2e6ddd17bcc2
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-04-27 12:41:58 +0100

    All tests passed.

--- 2017/Day_19/main.py
+++ 2017/Day_19/main.py
@@ -41,30 +41,78 @@ PART 1: The little packet looks up at you, hoping you can help it find the way.
         without line wrapping.)
 """
 
+import string
+
 
 class Network:
     def __init__(self, datafile: str):
-        pass
+        self.plan = {}
+        self.loc = None
+        self.direct = 1j
 
-    def find_start(self):
-        """
-        Find the location that the path through the network starts at. Also set
-        the direction the path is progressing.
-        """
-        pass
+        with open(datafile, "r") as fp:
+            for line_idx, line in enumerate(fp.readlines()):
+                for char_idx, char in enumerate(line):
+                    if not char.isspace():
+                        self.plan[(char_idx, line_idx)] = char
+
+                        # Set the top left most char as the start point
+                        if self.loc is None:
+                            self.loc = (char_idx, line_idx)
 
     def step(self):
         """
         Move one step along the path through the network.
         """
-        pass
+        nxt_loc = (self.loc[0] + self.direct.real, self.loc[1] + self.direct.imag)
+        assert nxt_loc in self.plan
+
+        # Continue along in the same direction
+        if self.plan[nxt_loc] in ["-", "|"]:
+            self.loc = nxt_loc
+
+        # Work out where to go at a junction or letter
+        elif self.plan[nxt_loc] in "+" + string.ascii_uppercase:
+
+            # Find the occupied location other than the current
+            possible_locs = [
+                (nxt_loc[0], nxt_loc[1] - 1),
+                (nxt_loc[0] - 1, nxt_loc[1]),
+                (nxt_loc[0] + 1, nxt_loc[1]),
+                (nxt_loc[0], nxt_loc[1] + 1),
+            ]
+
+            # Remove the previous location from the possible next moves
+            possible_locs.remove(self.loc)
+
+            # Find which possible location that has a non-space value
+            for loc in possible_locs:
+                if loc in self.plan and loc != self.loc:
+                    self.loc = nxt_loc
+                    nxt_loc = loc
+                    self.direct = complex(
+                        nxt_loc[0] - self.loc[0], nxt_loc[1] - self.loc[1]
+                    )
+                    break
+
+            # Indicate there is nowhere left to go after the next step
+            else:
+                self.direct = 0
+                self.loc = nxt_loc
 
     def path_letters(self) -> str:
         """
         Follow the path through the network and collect the letters that are
         encountered.
         """
-        pass
+        seen = ""
+
+        while self.direct != 0:
+            if self.plan[self.loc] in string.ascii_uppercase:
+                seen += self.plan[self.loc]
+            self.step()
+
+        return seen + self.plan[self.loc]
 
 
 if __name__ == "__main__":
--- 2017/Day_19/tests/test_main.py
+++ 2017/Day_19/tests/test_main.py
@@ -7,63 +7,88 @@ from main import Network
 
 def test_parse_data_exp01():
     test = Network("./data/example_01.txt")
-    assert test.plan == [
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", "|"],
-        [" ", " ", " ", " ", " ", "|", " ", " ", "+", "-", "-", "+"],
-        [" ", " ", " ", " ", " ", "A", " ", " ", "|", " ", " ", "C"],
-        [" ", "F", "-", "-", "-", "|", "-", "-", "-", "-", "E", "|", "-", "-", "+"],
-        [" ", " ", " ", " ", " ", "|", " ", " ", "|", " ", " ", "|", " ", " ", "D"],
-        [" ", " ", " ", " ", " ", "+", "B", "-", "+", " ", " ", "+", "-", "-", "+"],
-    ]
-
-
-def test_find_start_exp01():
-    test = Network("./data/example_01.txt")
-    test.find_start()
+    assert test.plan == {
+        (5, 0): "|",
+        (5, 1): "|",
+        (5, 2): "A",
+        (5, 3): "|",
+        (5, 4): "|",
+        (5, 5): "+",
+        (6, 5): "B",
+        (7, 5): "-",
+        (8, 5): "+",
+        (8, 4): "|",
+        (8, 3): "-",
+        (8, 2): "|",
+        (8, 1): "+",
+        (9, 1): "-",
+        (10, 1): "-",
+        (11, 1): "+",
+        (11, 2): "C",
+        (11, 3): "|",
+        (11, 4): "|",
+        (11, 5): "+",
+        (12, 5): "-",
+        (13, 5): "-",
+        (14, 5): "+",
+        (14, 4): "D",
+        (14, 3): "+",
+        (13, 3): "-",
+        (12, 3): "-",
+        (10, 3): "E",
+        (9, 3): "-",
+        (8, 3): "-",
+        (7, 3): "-",
+        (6, 3): "-",
+        (4, 3): "-",
+        (3, 3): "-",
+        (2, 3): "-",
+        (1, 3): "F",
+    }
     assert test.loc == (5, 0)
-    assert test.direct == -1j
+    assert test.direct == 1j
 
 
 def test_step_exp01():
     test = Network("./data/example_01.txt")
     test.loc = (5, 0)
-    test.direct = -1j
+    test.direct = 1j
     test.step()
     assert test.loc == (5, 1)
-    assert test.direct == -1j
+    assert test.direct == 1j
 
 
 def test_step_exp02():
     test = Network("./data/example_01.txt")
     test.loc = (5, 1)
-    test.direct = -1j
+    test.direct = 1j
     test.step()
     assert test.loc == (5, 2)
-    assert test.direct == -1j
+    assert test.direct == 1j
 
 
 def test_step_exp03():
     test = Network("./data/example_01.txt")
     test.loc = (5, 2)
-    test.direct = -1j
+    test.direct = 1j
     test.step()
     assert test.loc == (5, 3)
-    assert test.direct == -1j
+    assert test.direct == 1j
 
 
 def test_step_exp04():
     test = Network("./data/example_01.txt")
     test.loc = (5, 3)
-    test.direct = -1j
+    test.direct = 1j
     test.step()
     assert test.loc == (5, 4)
-    assert test.direct == -1j
+    assert test.direct == 1j
 
 
 def test_step_exp05():
     test = Network("./data/example_01.txt")
     test.loc = (5, 4)
-    test.direct = -1j
+    test.direct = 1j
     test.step()
     assert test.loc == (5, 5)
     assert test.direct == 1
@@ -93,40 +118,40 @@ def test_step_exp08():
     test.direct = 1
     test.step()
     assert test.loc == (8, 5)
-    assert test.direct == 1j
+    assert test.direct == -1j
 
 
 def test_step_exp09():
     test = Network("./data/example_01.txt")
     test.loc = (8, 5)
-    test.direct = 1j
+    test.direct = -1j
     test.step()
     assert test.loc == (8, 4)
-    assert test.direct == 1j
+    assert test.direct == -1j
 
 
 def test_step_exp10():
     test = Network("./data/example_01.txt")
     test.loc = (8, 4)
-    test.direct = 1j
+    test.direct = -1j
     test.step()
-    assert test.loc == (8, 4)
-    assert test.direct == 1j
+    assert test.loc == (8, 3)
+    assert test.direct == -1j
 
 
 def test_step_exp11():
     test = Network("./data/example_01.txt")
     test.loc = (8, 3)
-    test.direct = 1j
+    test.direct = -1j
     test.step()
-    assert test.loc == (8, 4)
-    assert test.direct == 1j
+    assert test.loc == (8, 2)
+    assert test.direct == -1j
 
 
 def test_step_exp12():
     test = Network("./data/example_01.txt")
     test.loc = (8, 2)
-    test.direct = 1j
+    test.direct = -1j
     test.step()
     assert test.loc == (8, 1)
     assert test.direct == 1
