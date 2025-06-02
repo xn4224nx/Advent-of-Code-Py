2fa5788810a136d77f663db7922640d7cacf9e62
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-06-02 20:28:16 +0100

    Part 2 completed.

--- 2018/Day_01/main.py
+++ 2018/Day_01/main.py
@@ -47,6 +47,35 @@ Here are other example situations:
 
 PART 1: Starting with a frequency of zero, what is the resulting frequency after
         all of the changes in frequency have been applied?
+
+You notice that the device repeats the same frequency change list over and over.
+To calibrate the device, you need to find the first frequency it reaches twice.
+
+For example, using the same list of changes above, the device would loop as
+follows:
+
+        -   Current frequency  0, change of +1; resulting frequency  1.
+        -   Current frequency  1, change of -2; resulting frequency -1.
+        -   Current frequency -1, change of +3; resulting frequency  2.
+        -   Current frequency  2, change of +1; resulting frequency  3.
+        -   (At this point, the device continues from the start of the list.)
+        -   Current frequency  3, change of +1; resulting frequency  4.
+        -   Current frequency  4, change of -2; resulting frequency  2, which
+            has already been seen.
+
+In this example, the first frequency reached twice is 2. Note that your device
+might need to repeat its list of frequency changes many times before a duplicate
+frequency is found, and that duplicates might be found while in the middle of
+processing the list.
+
+Here are other examples:
+
+        -   +1, -1 first reaches 0 twice.
+        -   +3, +3, +4, -2, -4 first reaches 10 twice.
+        -   -6, +3, +8, +5, -6 first reaches 5 twice.
+        -   +7, +7, -2, -7, -4 first reaches 14 twice.
+
+PART 2: What is the first frequency your device reaches twice?
 """
 
 
@@ -57,6 +86,29 @@ class FreqDist:
     def final_freq(self) -> int:
         return sum(self.changes)
 
+    def repeated_freq(self) -> int:
+        """
+        Using the changes what is the first frequency that is encountered
+        twice
+        """
+        freq = 0
+        seen_freqs = set()
+
+        # Go over the instructions multiple times
+        while True:
+            for chng in self.changes:
+
+                # Check if a frequency has been seen before otherwise record it
+                if freq in seen_freqs:
+                    return freq
+                else:
+                    seen_freqs.add(freq)
+
+                freq += chng
+
 
 if __name__ == "__main__":
-    print(f"Part 1 = {FreqDist("./data/input.txt").final_freq()}")
+    print(
+        f"Part 1 = {FreqDist("./data/input.txt").final_freq()}\n"
+        f"Part 2 = {FreqDist("./data/input.txt").repeated_freq()}\n"
+    )
--- 2018/Day_01/tests/test_main.py
+++ 2018/Day_01/tests/test_main.py
@@ -39,3 +39,19 @@ def test_final_freq_exp03():
 
 def test_final_freq_exp04():
     assert FreqDist("./data/example_04.txt").final_freq() == -6
+
+
+def test_repeated_freq_exp05():
+    assert FreqDist("./data/example_05.txt").repeated_freq() == 0
+
+
+def test_repeated_freq_exp06():
+    assert FreqDist("./data/example_06.txt").repeated_freq() == 10
+
+
+def test_repeated_freq_exp07():
+    assert FreqDist("./data/example_07.txt").repeated_freq() == 5
+
+
+def test_repeated_freq_exp08():
+    assert FreqDist("./data/example_08.txt").repeated_freq() == 14
