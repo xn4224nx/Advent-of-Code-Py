212c1924911917779c033d61470195fcd7a6884e
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-07-12 21:42:59 +0100

    All tests passed.

--- 2018/Day_06/main.py
+++ 2018/Day_06/main.py
@@ -69,23 +69,84 @@ in this example, the size of the largest area is 17.
 PART 1: What is the size of the largest area that isn't infinite?
 """
 
+import sys
+
 
 class MineField:
     def __init__(self, coord_file: str):
-        pass
+        self.mine_coords = []
+        self.field_size = [0, 0]
+
+        with open(coord_file, "r") as fp:
+            for line in fp.readlines():
+                pnt = tuple(map(int, line.split(", ", 1)))
+
+                # Determine the furthest reach of the minefield
+                for idx in range(2):
+                    if pnt[idx] > self.field_size[idx]:
+                        self.field_size[idx] = pnt[idx]
+
+                self.mine_coords.append(pnt)
+        self.field_size = tuple(self.field_size)
 
     def manhatt_dist(self, pnt0: (int, int), pnt1: (int, int)) -> int:
         """
         Calculate the Manhattan distance between two points,
         """
-        pass
+        return abs(pnt0[0] - pnt1[0]) + abs(pnt0[1] - pnt1[1])
 
     def largest_enclosed_space(self) -> int:
         """
         For the minefield determine the largest area within it that is not
         infinite.
         """
-        pass
+        near_sqrs = [1] * len(self.mine_coords)
+        inf_area = [False] * len(self.mine_coords)
+
+        # Check every square to find out what mine is closest
+        for x_idx in range(self.field_size[0]):
+            for y_idx in range(self.field_size[1]):
+                pnt = (x_idx, y_idx)
+
+                # The square the mine is on is always closest to it but that
+                # is aleardy counted in `near_sqrs`
+                if pnt in self.mine_coords:
+                    continue
+
+                # Determine the closest mine to this square
+                min_dist = sys.maxsize
+                closest_mine = 0
+
+                for mine_idx in range(len(self.mine_coords)):
+                    dist = self.manhatt_dist(pnt, self.mine_coords[mine_idx])
+
+                    if dist < min_dist:
+                        min_dist = dist
+                        closest_mine = mine_idx
+
+                # If this point is on the edge then the area cover by that mine
+                # is infinite and it is excluded from the answer.
+                if not inf_area[closest_mine] and (
+                    pnt[0] == 0
+                    or pnt[0] == self.field_size[0]
+                    or pnt[1] == 0
+                    or pnt[1] == self.field_size[1]
+                ):
+                    inf_area[closest_mine] = True
+
+                # Increment the number of squares this mine is closest to
+                near_sqrs[closest_mine] += 1
+
+        # Find and return the mine with the largest enclosed area
+        max_area = 0
+        for mine_idx in range(len(self.mine_coords)):
+            if inf_area[mine_idx]:
+                continue
+
+            if not inf_area[mine_idx] and near_sqrs[mine_idx] > max_area:
+                max_area = near_sqrs[mine_idx]
+
+        return max_area
 
 
 if __name__ == "__main__":
