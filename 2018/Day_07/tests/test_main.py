7146e81d1e38dcd8b40f066ad697bc717fd160bc
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-07-13 15:36:09 +0100

    Setup for day 7.

---
+++ 2018/Day_07/tests/test_main.py
@@ -0,0 +1,21 @@
+"""
+Tests for the main script
+"""
+
+from main import SleighSteps
+
+
+def test_new_sleighsteps_exp01():
+    test = SleighSteps("./data/example_0.txt")
+    assert test.depends == {
+        0: {2},
+        1: {0},
+        2: set(),
+        3: {0},
+        4: {1, 3, 5},
+        5: {2},
+    }
+
+
+def test_correct_order_exp01():
+    assert SleighSteps("./data/example_0.txt").correct_order() == "CABDFE"
