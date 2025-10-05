7c29e9c79bcf2b8e2f8b20220bf20ec20b6e1538
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-10-05 20:47:16 +0100

    Part 2 completed.

--- 2019/Day_04/main.py
+++ 2019/Day_04/main.py
@@ -26,28 +26,58 @@ Other than the range rule, the following are true:
 
 PART 1: How many different passwords within the range given in your puzzle
         input meet these criteria?
+
+An Elf just remembered one more important detail: the two adjacent matching
+digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the
+following are now true:
+
+    -   112233 meets these criteria because the digits never decrease and all
+        repeated digits are exactly two digits long.
+
+    -   123444 no longer meets the criteria (the repeated 44 is part of a larger
+        group of 444).
+
+    -   111122 meets the criteria (even though 1 is repeated more than twice, it
+        still contains a double 22).
+
+PART 2: How many different passwords within the range given in your puzzle input
+        meet all of the criteria?
 """
 
 
-def valid_password(sec_num: int) -> bool:
+def valid_password(sec_num: int, single_double: bool = False) -> bool:
     """
     Apply rules to determine if this number could be a valid password.
     """
     num = str(sec_num)
     adj_digits = False
 
-    for digit_idx in range(1, len(num)):
+    for idx in range(1, len(num)):
 
-        # At least two adjacent digits must be identical
-        if not adj_digits and num[digit_idx] == num[digit_idx - 1]:
-            adj_digits = True
-
-        # Going from left to right, the digits never decrease
-        if ord(num[digit_idx - 1]) > ord(num[digit_idx]):
+        # Going from left to right, ensure the digits never decrease
+        if ord(num[idx - 1]) > ord(num[idx]):
             return False
+
+        # Detect a pair of identical numbers
+        if num[idx] == num[idx - 1]:
+
+            # Check the number before this pair
+            if single_double and idx > 1 and num[idx - 2] == num[idx - 1]:
+                continue
+
+            # Check the number after this pair
+            if single_double and idx < len(num) - 1 and num[idx + 1] == num[idx]:
+                continue
+
+            adj_digits = True
 
     return adj_digits
 
 
 if __name__ == "__main__":
-    print(f"Part 1 = {sum([valid_password(x) for x in range(254032, 789860)])}")
+    print(
+        f"Part 1 = {sum([valid_password(x) for x in range(254032, 789860)])}\n"
+        f"Part 2 = {sum([valid_password(x, True) for x in range(254032, 789860)])}\n"
+    )
--- 2019/Day_04/tests/test_main.py
+++ 2019/Day_04/tests/test_main.py
@@ -15,3 +15,15 @@ def test_pass_valid_exp_1():
 
 def test_pass_valid_exp_2():
     assert valid_password(123789) == False
+
+
+def test_pass_valid_exp_3():
+    assert valid_password(112233, True) == True
+
+
+def test_pass_valid_exp_4():
+    assert valid_password(123444, True) == False
+
+
+def test_pass_valid_exp_5():
+    assert valid_password(111122, True) == True
