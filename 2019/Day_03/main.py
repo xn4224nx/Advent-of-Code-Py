59307de7d12b1a347c57088f1363c531d8b62210
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-10-05 18:56:07 +0100

    Part 2 completed.

--- 2019/Day_03/main.py
+++ 2019/Day_03/main.py
@@ -60,6 +60,50 @@ Here are a few more examples:
 
 PART 1: What is the Manhattan distance from the central port to the closest
         intersection?
+
+It turns out that this circuit is very timing-sensitive; you actually need to
+minimize the signal delay.
+
+To do this, calculate the number of steps each wire takes to reach each
+intersection; choose the intersection where the sum of both wires' steps is
+lowest. If a wire visits a position on the grid multiple times, use the steps
+value from the first time it visits that position when calculating the total
+value of a specific intersection.
+
+The number of steps a wire takes is the total number of grid squares the wire
+has entered to get to that location, including the intersection being
+considered. Again consider the example from above:
+
+    ...........
+    .+-----+...
+    .|.....|...
+    .|..+--X-+.
+    .|..|..|.|.
+    .|.-X--+.|.
+    .|..|....|.
+    .|.......|.
+    .o-------+.
+    ...........
+
+In the above example, the intersection closest to the central port is reached
+after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the second
+wire for a total of 20+20 = 40 steps.
+
+However, the top-right intersection is better: the first wire takes only 8+5+2 =
+15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 steps.
+
+Here are the best steps for the extra examples from above:
+
+        -   R75,D30,R83,U83,L12,D49,R71,U7,L72
+            U62,R66,U55,R34,D71,R55,D58,R83
+            610 steps
+
+        -   R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+            U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+            410 steps
+
+PART 2: What is the fewest combined steps the wires must take to reach an intersection?
+
 """
 
 import sys
@@ -68,18 +112,22 @@ import sys
 class TwoWires:
     def __init__(self, direct_file: str):
         self.wire_pnts = []
+        self.wire_dists = []
 
         # Parse the directions and determine the points that each wire passes through
         with open(direct_file, "r") as fp:
             for line in fp.readlines():
-                pnt = (0, 0)
                 wire_pnts = set()
+                wire_dists = {}
+                pnt = (0, 0)
+                wire_dist = 0
 
                 for move in line.split(","):
                     direct = move[0]
                     magnit = int(move[1:])
 
                     for mv_idx in range(magnit):
+                        wire_dist += 1
 
                         if direct == "U":
                             pnt = (pnt[0], pnt[1] + 1)
@@ -97,9 +145,16 @@ class TwoWires:
                             raise Exception(f"Unknown direction: '{direct}'")
 
                         wire_pnts.add(pnt)
-                self.wire_pnts.append(wire_pnts)
 
-    def crossing_dist(self) -> int:
+                        # Only include the first wire len distance
+                        if pnt not in wire_dists:
+                            wire_dists[pnt] = wire_dist
+
+                # Record the stats for this wire
+                self.wire_pnts.append(wire_pnts)
+                self.wire_dists.append(wire_dists)
+
+    def crossing_dist(self, wire_dist: bool = False) -> int:
         """
         What is the closest distance to an intersection of the two wires.
         """
@@ -107,7 +162,10 @@ class TwoWires:
 
         # Find all the intersecting points and find the closest one
         for pnt in self.wire_pnts[0].intersection(self.wire_pnts[1]):
-            dist = abs(pnt[0]) + abs(pnt[1])
+            if wire_dist:
+                dist = self.wire_dists[0][pnt] + self.wire_dists[1][pnt]
+            else:
+                dist = abs(pnt[0]) + abs(pnt[1])
 
             if dist < min_dist:
                 min_dist = dist
@@ -116,4 +174,7 @@ class TwoWires:
 
 
 if __name__ == "__main__":
-    print(f"Part 1 = {TwoWires('./data/input_0.txt').crossing_dist()}")
+    print(
+        f"Part 1 = {TwoWires('./data/input_0.txt').crossing_dist()}\n"
+        f"Part 2 = {TwoWires('./data/input_0.txt').crossing_dist(True)}\n"
+    )
--- 2019/Day_03/tests/test_main.py
+++ 2019/Day_03/tests/test_main.py
@@ -55,6 +55,54 @@ def test_new_two_wires_exp_0():
             (6, 3),
         },
     ]
+    assert test.wire_dists == [
+        {
+            (1, 0): 1,
+            (2, 0): 2,
+            (3, 0): 3,
+            (4, 0): 4,
+            (5, 0): 5,
+            (6, 0): 6,
+            (7, 0): 7,
+            (8, 0): 8,
+            (8, 1): 9,
+            (8, 2): 10,
+            (8, 3): 11,
+            (8, 4): 12,
+            (8, 5): 13,
+            (7, 5): 14,
+            (6, 5): 15,
+            (5, 5): 16,
+            (4, 5): 17,
+            (3, 5): 18,
+            (3, 4): 19,
+            (3, 3): 20,
+            (3, 2): 21,
+        },
+        {
+            (0, 1): 1,
+            (0, 2): 2,
+            (0, 3): 3,
+            (0, 4): 4,
+            (0, 5): 5,
+            (0, 6): 6,
+            (0, 7): 7,
+            (1, 7): 8,
+            (2, 7): 9,
+            (3, 7): 10,
+            (4, 7): 11,
+            (5, 7): 12,
+            (6, 7): 13,
+            (6, 6): 14,
+            (6, 5): 15,
+            (6, 4): 16,
+            (6, 3): 17,
+            (5, 3): 18,
+            (4, 3): 19,
+            (3, 3): 20,
+            (2, 3): 21,
+        },
+    ]
 
 
 def test_crossing_dist_exp_0():
@@ -67,3 +115,15 @@ def test_crossing_dist_exp_1():
 
 def test_crossing_dist_exp_2():
     assert TwoWires("./data/example_2.txt").crossing_dist() == 135
+
+
+def test_crossing_dist_exp_3():
+    assert TwoWires("./data/example_0.txt").crossing_dist(True) == 30
+
+
+def test_crossing_dist_exp_4():
+    assert TwoWires("./data/example_1.txt").crossing_dist(True) == 610
+
+
+def test_crossing_dist_exp_5():
+    assert TwoWires("./data/example_2.txt").crossing_dist(True) == 410
