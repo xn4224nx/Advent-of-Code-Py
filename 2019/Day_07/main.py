0fe875a9b0a49cb0e4c2d7fc721dbb999157c4fb
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-10-26 16:46:25 +0000

    All tests passed.

--- 2019/Day_07/main.py
+++ 2019/Day_07/main.py
@@ -82,11 +82,14 @@ PART 1: Try every combination of phase settings on the amplifiers. What is the
         highest signal that can be sent to the thrusters?
 """
 
+from itertools import permutations
+
 
 class Amplifier:
     def __init__(self, program_file: str):
         self.ptr = 0
-        self.input_code = None
+        self.phase_setting = None
+        self.input_signal = None
         self.outputs = []
 
         # Read the initial state of the program memory
@@ -149,7 +152,16 @@ class Amplifier:
 
         # Opcode 3 - Set a value in memory to what the input is - one parameter
         elif opcode == 3:
-            self.mem[self.mem[self.ptr + 1]] = self.input_code
+
+            # At the first input instruction provide the phase setting
+            if self.phase_setting is not None:
+                self.mem[self.mem[self.ptr + 1]] = self.phase_setting
+                self.phase_setting = None
+
+            # Otherwise provide the input code
+            else:
+                self.mem[self.mem[self.ptr + 1]] = self.input_signal
+
             self.ptr += 2
 
         # Opcode 4 - Output a value - one parameter
@@ -192,12 +204,13 @@ class Amplifier:
 
         return opcode
 
-    def final_output(self, input_value: int) -> int:
+    def final_output(self, input_signal: int, phase_setting: int) -> int:
         """
         For a program input a specific value and return the value the program
         outputs.
         """
-        self.input_code = input_instr
+        self.phase_setting = phase_setting
+        self.input_signal = input_signal
 
         # Set the program to its intial state
         self.ptr = 0
@@ -210,25 +223,33 @@ class Amplifier:
 
         return self.outputs[-1]
 
-    def run_phase_setting(self, setting: list[int]) -> int:
+    def run_phase_setting(self, settings: list[int]) -> int:
         """
         What is the final thruster signal value from a specific phase setting?
         """
-        pass
+        input_value = 0
 
-    def max_amp_comb(self) -> list[int]:
+        for p_setting in settings:
+            input_value = self.final_output(input_value, p_setting)
+
+        return input_value
+
+    def max_thruster_signal(self) -> (int, list[int]):
         """
         What is the combination of phase settings that will produce the largest
         thruster value from this Amplifier controller software?
         """
-        pass
+        max_comb = None
+        max_thrust = 0
 
-    def max_thruster_signal(self) -> int:
-        """
-        What is the largest thruster value possible from this Amplifier
-        controller software?
-        """
-        pass
+        for phase_comb in permutations(range(5)):
+            curr_thrust = self.run_phase_setting(list(phase_comb))
+
+            if curr_thrust > max_thrust:
+                max_thrust = curr_thrust
+                max_comb = list(phase_comb)
+
+        return max_thrust, max_comb
 
 
 if __name__ == "__main__":
