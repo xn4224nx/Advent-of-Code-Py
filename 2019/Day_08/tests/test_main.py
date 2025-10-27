1a5f45cec2a64918fdfe6ae2323272223465b905
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-10-27 22:20:17 +0000

    Setup for day 8.

---
+++ 2019/Day_08/tests/test_main.py
@@ -0,0 +1,15 @@
+"""
+Tests for the main script.
+"""
+
+from main import DSNImage
+
+
+def test_new_img_exp0():
+    test = DSNImage("./data/example_0.txt", (3, 2))
+    assert test.dims == (3, 2)
+    assert test.pixels == [[1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 1, 2]]
+
+
+def test_checksum_exp0():
+    assert DSNImage("./data/example_0.txt", (3, 2)).checksum() == 1
